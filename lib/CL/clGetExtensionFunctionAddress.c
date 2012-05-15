2de41e2b9d42123801505490983f6f9131e690f9
Kalle Raiskila <kraiskil@ovi.com>
2012-05-15 20:09:53 +0000

    Pocl registers with AMD'd ICD loader. (previous commit's message was a missunderstanding). 'clinfo' still segfaults.

--- lib/CL/clGetExtensionFunctionAddress.c
+++ lib/CL/clGetExtensionFunctionAddress.c
@@ -7,8 +7,11 @@ CL_API_ENTRY void * CL_API_CALL
 clGetExtensionFunctionAddress(const char * func_name ) 
 CL_EXT_SUFFIX__VERSION_1_0
 {
-  // return the address of the function "func_name"
-  // no extensions in POCL!
-  return NULL;
+
+  // TODO: check if there is a (need for) a macro for string-switches
+  if( strcmp(func_name, "clIcdGetPlatformIDsKHR")==0 )
+    return (void *)&clIcdGetPlatformIDsKHR;
+  else
+    return NULL;
 }
 
