8235ec977625370e969ca876d8e6f15224bd2159
Cyril Roelandt <tipecaml@gmail.com>
2012-06-13 01:46:43 +0200

    Fill in the global_mem_size and max_mem_alloc_size fields of the cl_device_id structure when initializing a basic or pthread device.

---
+++ lib/CL/devices/topology/pocl_topology.h
@@ -0,0 +1,6 @@
+#ifndef POCL_TOPOLOGY_H
+#define POCL_TOPOLOGY_H
+
+void pocl_topology_set_global_mem_size(cl_device_id device);
+void pocl_topology_set_max_mem_alloc_size(cl_device_id device);
+#endif /* POCL_TOPOLOGY_H */
