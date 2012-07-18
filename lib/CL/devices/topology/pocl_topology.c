99c22feb1429febfde539a11c0c4c9369b05c0c5
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2012-07-18 20:50:22 +0300

    Embedded the hwloc lib (1.4.2) to drop the dependency using the instructions from http://www.open-mpi.org/projects/hwloc/doc/v1.4.2/a00010.php

--- lib/CL/devices/topology/pocl_topology.c
+++ lib/CL/devices/topology/pocl_topology.c
@@ -1,4 +1,4 @@
-/* pocl_topology.c - retrieving the topology of OpenCL devices
+/* pocl_topology.c - retrieving the topology of OpenCL devices using the hwloc
 
    Copyright (c) 2012 Cyril Roelandt
    
@@ -21,8 +21,8 @@
    THE SOFTWARE.
 */
 
-#include <pocl_cl.h>
 #include <hwloc.h>
+#include <pocl_cl.h>
 
 /* We may want to protect these with a mutex, but it's probably not needed for
  * now. */
@@ -34,10 +34,10 @@ pocl_topology_init(void)
 {
   int ret;
 
-  ret = hwloc_topology_init(&pocl_topology);
+  ret = pocl_hwloc_topology_init(&pocl_topology);
   if (ret == -1)
     POCL_ABORT("Cannot initialize the topology.\n");
-  ret = hwloc_topology_load(pocl_topology);
+  ret = pocl_hwloc_topology_load(pocl_topology);
   if (ret == -1)
     POCL_ABORT("Cannot load the topology.\n");
 
@@ -51,7 +51,8 @@ pocl_topology_set_global_mem_size(cl_device_id device)
   if (!init)
     pocl_topology_init();
 
-  device->global_mem_size = hwloc_get_root_obj(pocl_topology)->memory.total_memory;
+  device->global_mem_size = 
+    pocl_hwloc_get_root_obj(pocl_topology)->memory.total_memory;
 }
 
 void
