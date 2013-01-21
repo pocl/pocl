6483e1f3e2f78978e59c2d6489844a86c140d474
Kalle Raiskila <kraiskil@ovi.com>
2013-01-21 15:03:16 +0200

    Fix powerpc build.

--- lib/CL/devices/basic/basic.h
+++ lib/CL/devices/basic/basic.h
@@ -129,7 +129,7 @@ GEN_PROTOTYPES (basic)
   NULL, /* build_program */ \
   NULL, /* data */                                      \
   KERNEL_DIR,  /* kernel_lib_target (forced kernel library dir) */    \
-  HOST, /* llvm_target_triplet */                           \
+  OCL_KERNEL_TARGET, /* llvm_target_triplet */                           \
   0     /* dev_id */           \
 }
 
