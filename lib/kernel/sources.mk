afb461d9fe0d7ee5e7698f54112ee7ab758b1e40
Erik Schnetter <eschnetter@perimeterinstitute.ca>
2013-07-04 15:25:01 -0400

    Simplify kernel build logic
    Remove logic for special case where Vecmathlib would override
    architecture specific files. This works because no such architecture
    specific files exist any more.
    
    Update list of source files that should be provided by Vecmathlib.
    
    For object file names, keep source file suffix and only append .bc.

--- lib/kernel/sources.mk
+++ lib/kernel/sources.mk
@@ -23,63 +23,81 @@
 
 CLANGFLAGS = -emit-llvm
 
-# Search the .cl,.c and .ll sources first from this (target specific) directory, then
-# the one-up (generic) directory. This allows to override the generic implementation 
-# simply by adding a similarly named file in the target specific directory
+# Search the .cl, .c, and .ll sources first from this (target
+# specific) directory, then the one-up (generic) directory. This
+# allows to override the generic implementation simply by adding a
+# similarly named file in the target specific directory
 
-# NOTE: GNU make always searches from '.' first which can be problematic in case
-# one wants to override the files in the device-specific directory. To circumvent
-# this one can override a default file with a more accurate path, e.g.,
-# vecmathlib/pocl/fmax.cl. This overrides a possible fmax.cl found in the current
-# directory by one from the VML.  TODO: dependency tracking does not work
-# in that case because the .bc is not created in the directory where the source is.
-vpath %.cl @srcdir@:${SECONDARY_VPATH}:@srcdir@/..
+# NOTE: GNU make always searches from '.' first, which can be
+# problematic in case one wants to override the files in the
+# device-specific directory. To circumvent this, one can override a
+# default file with a more accurate path, e.g.,
+# vecmathlib/pocl/fmax.cl. This overrides a possible fmax.cl found in
+# the current directory by one from the VML. TODO: dependency tracking
+# does not work in that case because the .bc is not created in the
+# directory where the source is.
+vpath %.h @top_srcdir@/include:@srcdir@:${SECONDARY_VPATH}:@srcdir@/..
 vpath %.c @srcdir@:${SECONDARY_VPATH}:@srcdir@/..
+vpath %.cc @srcdir@:${SECONDARY_VPATH}:@srcdir@/..
+vpath %.cl @srcdir@:${SECONDARY_VPATH}:@srcdir@/..
 vpath %.ll @srcdir@:${SECONDARY_VPATH}:@srcdir@/..
-vpath %.h @top_srcdir@/include:@srcdir@:@srcdir@/..
-vpath %.cc @srcdir@:@srcdir@/../vecmathlib/pocl:@srcdir@/..
 
-LKERNEL_HDRS=templates.h image.h
-# Nodist here because these files should be included
-# to the distribution only once, from the root kernel
-# makefile.
-LKERNEL_SRCS_DEFAULT= \
+LKERNEL_HDRS = image.h templates.h
+LKERNEL_SRCS_DEFAULT=				\
 	barrier.ll				\
-	get_work_dim.c				\
-	get_global_size.c			\
 	get_global_id.c				\
-	get_local_size.c			\
-	get_local_id.c				\
-	get_num_groups.c			\
-	get_group_id.c				\
 	get_global_offset.c			\
-	as_type.cl				\
-	atomics.cl				\
+	get_global_size.c			\
+	get_group_id.c				\
+	get_local_id.c				\
+	get_local_size.c			\
+	get_num_groups.c			\
+	get_work_dim.c				\
+	abs.cl					\
+	abs_diff.cl				\
 	acos.cl					\
 	acosh.cl				\
 	acospi.cl				\
+	add_sat.cl				\
+	all.cl					\
+	any.cl					\
+	as_type.cl				\
 	asin.cl					\
 	asinh.cl				\
 	asinpi.cl				\
+	async_work_group_copy.cl		\
 	atan.cl					\
 	atan2.cl				\
 	atan2pi.cl				\
 	atanh.cl				\
 	atanpi.cl				\
+	atomics.cl				\
+	bitselect.cl				\
 	cbrt.cl					\
 	ceil.cl					\
+	clamp.cl				\
+	clamp_int.cl				\
+	clz.cl					\
 	convert_type.cl				\
 	copysign.cl				\
 	cos.cl					\
 	cosh.cl					\
 	cospi.cl				\
-	erfc.cl					\
+	cross.cl				\
+	degrees.cl				\
+	distance.cl				\
+	divide.cl				\
+	dot.cl					\
 	erf.cl					\
+	erfc.cl					\
 	exp.cl					\
-	exp2.cl					\
 	exp10.cl				\
+	exp2.cl					\
 	expm1.cl				\
 	fabs.cl					\
+	fast_distance.cl			\
+	fast_length.cl				\
+	fast_normalize.cl			\
 	fdim.cl					\
 	floor.cl				\
 	fma.cl					\
@@ -87,136 +105,111 @@ LKERNEL_SRCS_DEFAULT= \
 	fmin.cl					\
 	fmod.cl					\
 	fract.cl				\
+	get_image_height.cl			\
+	get_image_width.cl			\
+	hadd.cl					\
 	hypot.cl				\
 	ilogb.cl				\
+	isequal.cl				\
+	isfinite.cl				\
+	isgreater.cl				\
+	isgreaterequal.cl			\
+	isinf.cl				\
+	isless.cl				\
+	islessequal.cl				\
+	islessgreater.cl			\
+	isnan.cl				\
+	isnormal.cl				\
+	isnotequal.cl				\
+	isordered.cl				\
+	isunordered.cl				\
 	ldexp.cl				\
+	length.cl				\
 	lgamma.cl				\
 	log.cl					\
-	log2.cl					\
 	log10.cl				\
 	log1p.cl				\
+	log2.cl					\
 	logb.cl					\
 	mad.cl					\
+	mad24.cl				\
+	mad_hi.cl				\
+	mad_sat.cl				\
+	max.cl					\
+	max_i.cl				\
 	maxmag.cl				\
+	min.cl					\
+	min_i.cl				\
 	minmag.cl				\
+	mix.cl					\
+	mul24.cl				\
+	mul_hi.cl				\
 	nan.cl					\
-	native_cos.cl 			\
-	native_log2.cl 			\
-	nextafter.cl			\
+	native_cos.cl				\
+	native_log2.cl				\
+	nextafter.cl				\
+	normalize.cl				\
+	popcount.cl				\
 	pow.cl					\
 	pown.cl					\
 	powr.cl					\
+	radians.cl				\
+	read_image.cl				\
+	recip.cl				\
 	remainder.cl				\
+	rhadd.cl				\
 	rint.cl					\
 	rootn.cl				\
+	rotate.cl				\
 	round.cl				\
 	rsqrt.cl				\
+	select.cl				\
+	sign.cl					\
+	signbit.cl				\
 	sin.cl					\
 	sincos.cl				\
 	sinh.cl					\
 	sinpi.cl				\
+	smoothstep.cl				\
 	sqrt.cl					\
+	step.cl					\
+	sub_sat.cl				\
 	tan.cl					\
 	tanh.cl					\
 	tanpi.cl				\
 	tgamma.cl				\
 	trunc.cl				\
-	divide.cl				\
-	recip.cl				\
-	abs.cl					\
-	abs_diff.cl				\
-	add_sat.cl				\
-	hadd.cl					\
-	rhadd.cl				\
-	clamp.cl				\
-	clamp_int.cl				\
-	clz.cl					\
-	mad_hi.cl				\
-	mad_sat.cl				\
-	max.cl					\
-	min.cl					\
-	mul_hi.cl				\
-	rotate.cl				\
-	sub_sat.cl				\
 	upsample.cl				\
-	popcount.cl				\
-	mad24.cl				\
-	mul24.cl				\
-	degrees.cl				\
-	mix.cl					\
-	radians.cl				\
-	step.cl					\
-	smoothstep.cl				\
-	sign.cl					\
-	cross.cl				\
-	dot.cl					\
-	distance.cl				\
-	length.cl				\
-	normalize.cl				\
-	fast_distance.cl			\
-	fast_length.cl				\
-	fast_normalize.cl			\
-	isequal.cl				\
-	isnotequal.cl				\
-	isgreater.cl				\
-	isgreaterequal.cl			\
-	isless.cl				\
-	islessequal.cl				\
-	islessgreater.cl			\
-	isfinite.cl				\
-	isinf.cl				\
-	isnan.cl				\
-	isnormal.cl				\
-	isordered.cl				\
-	isunordered.cl				\
-	signbit.cl				\
-	any.cl					\
-	all.cl					\
-	bitselect.cl				\
-	select.cl				\
 	vload.cl				\
-	vstore.cl				\
 	vload_half.cl				\
+	vstore.cl				\
 	vstore_half.cl				\
-	async_work_group_copy.cl		\
 	wait_group_events.cl			\
-	read_image.cl				\
-	write_image.cl				\
-	get_image_width.cl			\
-	get_image_height.cl     
+	write_image.cl
 
 # The standard list of kernel sources can be modified with
-# EXCLUDE_SRC_FILES, which removes files from the standard list, and
-# LKERNEL_EXTRA_SRCS, which adds extra files to the source list.
-LKERNEL_SRCS = $(filter-out ${EXCLUDE_SRC_FILES}, ${LKERNEL_SRCS_DEFAULT} ) \
-	${LKERNEL_EXTRA_SRCS}
+# LKERNEL_SRCS_EXCLUDE, which removes files from the standard list,
+# and LKERNEL_SRCS_EXTRA, which adds extra files to the source list.
+LKERNEL_SRCS =								\
+	$(filter-out ${LKERNEL_SRCS_EXCLUDE}, ${LKERNEL_SRCS_DEFAULT})	\
+	${LKERNEL_SRCS_EXTRA}
 
-# TODO: why are .cc files compiled to .cc.bc, while all other files
-# are compiled to .bc?
-OBJ_L=$(LKERNEL_SRCS:.cl=.bc)
-OBJ_C=$(OBJ_L:.ll=.bc)
-OBJ_CC=$(OBJ_C:.cc=.cc.bc)
+OBJ = $(LKERNEL_SRCS:%=%.bc)
 
-OBJ=$(OBJ_CC:.c=.bc)
-
-OBJ:LKERNEL_SRCS
-
-#libkernel_SRCS = $LIBKERNEL_SOURCES
-
-#rules to compile the different kernel library source file types into LLVM bitcode
-%.bc: %.cl @top_builddir@/include/${TARGET_DIR}/types.h @top_srcdir@/include/_kernel.h
-	@CLANG@ -Xclang -ffake-address-space-map -emit-llvm ${CLFLAGS} ${EXTRA_CLANGFLAGS} -fsigned-char -c -target ${KERNEL_TARGET} -o ${notdir $@} -x cl $< -include ../../../include/${TARGET_DIR}/types.h -include ${abs_top_srcdir}/include/_kernel.h
-%.bc: %.c @top_builddir@/include/${TARGET_DIR}/types.h
+# Rules to compile the different kernel library source file types into
+# LLVM bitcode
+%.c.bc: %.c @top_builddir@/include/${TARGET_DIR}/types.h
 	@CLANG@ -Xclang -ffake-address-space-map -emit-llvm ${CLFLAGS} ${EXTRA_CLANGFLAGS} -c -target ${KERNEL_TARGET} -o ${notdir $@} -x c $< -include ../../../include/${TARGET_DIR}/types.h
 %.cc.bc: %.cc @top_builddir@/include/${TARGET_DIR}/types.h
 	@CLANGXX@ -Xclang -ffake-address-space-map -std=c++11 -fno-exceptions -emit-llvm ${EXTRA_CLANGFLAGS} ${CLANGXX_FLAGS} -c -target ${KERNEL_TARGET} -o ${notdir $@} $< -include ../../../include/${TARGET_DIR}/types.h
+%.cl.bc: %.cl @top_builddir@/include/${TARGET_DIR}/types.h @top_srcdir@/include/_kernel.h
+	@CLANG@ -Xclang -ffake-address-space-map -emit-llvm ${CLFLAGS} ${EXTRA_CLANGFLAGS} -fsigned-char -c -target ${KERNEL_TARGET} -o ${notdir $@} -x cl $< -include ../../../include/${TARGET_DIR}/types.h -include ${abs_top_srcdir}/include/_kernel.h
+%.ll.bc: %.ll
+	@LLVM_AS@ -o $@ $<
 
 CLEANFILES = kernel-${KERNEL_TARGET}.bc ${notdir ${OBJ}}
 
-# Optimize the bitcode library to speed up optimization times for the OpenCL kernels.
+# Optimize the bitcode library to speed up optimization times for the
+# OpenCL kernels
 kernel-${KERNEL_TARGET}.bc: ${OBJ}
 	@LLVM_LINK@ ${notdir $^} -o - | @LLVM_OPT@ ${KERNEL_LIB_OPT_FLAGS} -O3 -o $@
-
-# We need an explicitly rule to overwrite automake guess about LEX file :-(
-barrier.bc: barrier.ll
-	@LLVM_AS@ -o $@ $<
