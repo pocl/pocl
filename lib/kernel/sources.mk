869fb29d2186f8848a58e169b5475167394d207c
Erik Schnetter <eschnetter@perimeterinstitute.ca>
2013-07-09 19:45:57 -0400

    Do not use -std=c++11
    Clang does not support all C++11 features, even when this option is
    given. By default, it still usese the system's C++ library, which is
    often not C++11. It is cleaner to not make any code expect that C++11
    is supported.

--- lib/kernel/sources.mk
+++ lib/kernel/sources.mk
@@ -201,7 +201,7 @@ OBJ = $(LKERNEL_SRCS:%=%.bc)
 %.c.bc: %.c @top_builddir@/include/${TARGET_DIR}/types.h
 	@CLANG@ -Xclang -ffake-address-space-map -emit-llvm ${CLFLAGS} ${EXTRA_CLANGFLAGS} -c -target ${KERNEL_TARGET} -o ${notdir $@} -x c $< -include ../../../include/${TARGET_DIR}/types.h
 %.cc.bc: %.cc @top_builddir@/include/${TARGET_DIR}/types.h
-	@CLANGXX@ -Xclang -ffake-address-space-map -std=c++11 -fno-exceptions -emit-llvm ${EXTRA_CLANGFLAGS} ${CLANGXX_FLAGS} -c -target ${KERNEL_TARGET} -o ${notdir $@} $< -include ../../../include/${TARGET_DIR}/types.h
+	@CLANGXX@ -Xclang -ffake-address-space-map -fno-exceptions -emit-llvm ${EXTRA_CLANGFLAGS} ${CLANGXX_FLAGS} -c -target ${KERNEL_TARGET} -o ${notdir $@} $< -include ../../../include/${TARGET_DIR}/types.h
 %.cl.bc: %.cl @top_builddir@/include/${TARGET_DIR}/types.h @top_srcdir@/include/_kernel.h
 	@CLANG@ -Xclang -ffake-address-space-map -emit-llvm ${CLFLAGS} ${EXTRA_CLANGFLAGS} -fsigned-char -c -target ${KERNEL_TARGET} -o ${notdir $@} -x cl $< -include ../../../include/${TARGET_DIR}/types.h -include ${abs_top_srcdir}/include/_kernel.h
 %.ll.bc: %.ll
