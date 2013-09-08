d3bed01b86656a78989e4205d999df6db90f0d53
Erik Schnetter <schnetter@gmail.com>
2013-09-08 14:30:21 -0400

    Move comment

--- include/pocl_features.h
+++ include/pocl_features.h
@@ -1,14 +1,11 @@
 // Supported datataypes
 
-// Note: The definitions of "long" below differs between languages. We
-// therefore check "long long" instead.
-
-
-
 // LLVM always supports fp16 (aka half)
 #define cl_khr_fp16
 
 // Is long supported?
+// Note: The definitions of "long" below differs between languages. We
+// therefore need to check "long long" instead as well.
 #if __SIZEOF_LONG__ == 8 || __SIZEOF_LONG_LONG__ == 8
 #  define cles_khr_int64
 #else
