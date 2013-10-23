b430fafc3f57105e5c96f936e7a62b29117e8586
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2013-10-23 08:42:52 +0300

    Add removed TCE type definitions back.

---
+++ include/tce/types.h
@@ -0,0 +1,13 @@
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+
+#define __EMBEDDED_PROFILE__ 1
+#undef cles_khr_int64
+#define cl_khr_fp16
+#undef cl_khr_fp64
+
+typedef uint size_t;
+typedef int ptrdiff_t;
+typedef int intptr_t;
+typedef uint uintptr_t;
