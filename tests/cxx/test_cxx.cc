748b5bb09f9cc2d6970994f8f4ec990a6a291604
Erik Schnetter <schnetter@gmail.com>
2013-11-21 17:01:21 -0700

    Clean up test case

--- tests/cxx/test_cxx.cc
+++ tests/cxx/test_cxx.cc
@@ -1,18 +1,5 @@
 // TESTING: C++ kernels
 
-// #define __kernel
-
-// unsigned _test_cxx_NUM_ARGS     = 0;
-// int _test_cxx_ARG_IS_POINTER[]  = {};
-// int _test_cxx_ARG_IS_LOCAL[]    = {};
-// int _test_cxx_ARG_IS_IMAGE[]    = {};
-// int _test_cxx_ARG_IS_SAMPLER[]  = {};
-// int _test_cxx_REQD_WG_SIZE[3]   = {0, 0, 0};
-// unsigned _test_cxx_NUM_LOCALS   = 0;
-// unsigned _test_cxx_LOCAL_SIZE[] = {};
-
-
-
 #include <iostream>
 
 template<typename T>
