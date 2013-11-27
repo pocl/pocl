fa9937610d605decc165114ddb6355840adf4d46
Clement Leger <clement.leger@kalray.eu>
2013-11-27 11:12:47 +0100

    Devices: use designated initializers for device_id struct. This allow to easily add fields without breaking devices

--- lib/CL/devices/cellspu/cellspu.h
+++ lib/CL/devices/cellspu/cellspu.h
@@ -54,102 +54,63 @@ extern "C" {
 GEN_PROTOTYPES (cellspu)
 
 #define POCL_DEVICES_CELLSPU {						\
-  POCL_DEVICE_ICD_DISPATCH     						\
-  POCL_OBJECT_INIT, \
-  CL_DEVICE_TYPE_ACCELERATOR, /* type */					\
-  0, /* vendor_id */							\
-  1, /* max_compute_units */						\
-  3, /* max_work_item_dimensions */					\
-  {CL_INT_MAX, CL_INT_MAX, CL_INT_MAX}, /* max_work_item_sizes */		\
-  1024, /* max_work_group_size */					\
-  8, /* preferred_wg_size_multiple */                                \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_CHAR  , /* preferred_vector_width_char */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_SHORT , /* preferred_vector_width_short */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_INT   , /* preferred_vector_width_int */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_LONG  , /* preferred_vector_width_long */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_FLOAT , /* preferred_vector_width_float */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_DOUBLE, /* preferred_vector_width_double */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_HALF  , /* preferred_vector_width_half */ \
+  .type = CL_DEVICE_TYPE_ACCELERATOR, 					\
+  .max_compute_units = 1, 						\
+  .max_work_item_dimensions = 3, 					\
+  .max_work_item_sizes = {CL_INT_MAX, CL_INT_MAX, CL_INT_MAX}, 		\
+  .max_work_group_size = 1024, 					        \
+  .preferred_wg_size_multiple = 8,                                      \
+  .preferred_vector_width_char = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_CHAR  ,    \
+  .preferred_vector_width_short = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_SHORT ,   \
+  .preferred_vector_width_int = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_INT   ,     \
+  .preferred_vector_width_long = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_LONG  ,    \
+  .preferred_vector_width_float = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_FLOAT ,   \
+  .preferred_vector_width_double = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_DOUBLE,  \
+  .preferred_vector_width_half = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_HALF,      \
   /* TODO: figure out what the difference between preferred and native widths are. */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_CHAR  , /* preferred_vector_width_char */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_SHORT , /* preferred_vector_width_short */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_INT   , /* preferred_vector_width_int */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_LONG  , /* preferred_vector_width_long */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_FLOAT , /* preferred_vector_width_float */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_DOUBLE, /* preferred_vector_width_double */ \
-  POCL_DEVICES_PREFERRED_VECTOR_WIDTH_HALF  , /* preferred_vector_width_half */ \
-  100, /* max_clock_frequency */						\
-  0, /* address_bits */							\
-  0, /* max_mem_alloc_size */						\
-  CL_FALSE, /* image_support */						\
-  0, /* max_read_image_args */						\
-  0, /* max_write_image_args */						\
-  0, /* image2d_max_width */						\
-  0, /* image2d_max_height */						\
-  0, /* image3d_max_width */						\
-  0, /* image3d_max_height */						\
-  0, /* image3d_max_depth */						\
-  0, /* image_max_buffer_size */					\
-  0, /* image_max_array_size */						\
-  0, /* max_samplers */							\
-  0, /* max_parameter_size */						\
-  0, /* mem_base_addr_align */						\
-  0, /* min_data_type_align_size */					\
-  0, /* half_fp_config */	\
-  CL_FP_ROUND_TO_NEAREST | CL_FP_INF_NAN, /* single_fp_config */	\
-  CL_FP_ROUND_TO_NEAREST | CL_FP_INF_NAN, /* double_fp_config */       \
-  CL_NONE, /* global_mem_cache_type */					\
-  0, /* global_mem_cacheline_size */					\
-  0, /* global_mem_cache_size */					\
-  0, /* global_mem_size */						\
-  0, /* max_constant_buffer_size */					\
-  0, /* max_constant_args */						\
-  CL_GLOBAL, /* local_mem_type */					\
-  0, /* local_mem_size */						\
-  CL_FALSE, /* error_correction_support */				\
-  CL_TRUE, /* host_unified_memory */                \
-  0, /* profiling_timer_resolution */					\
-  CL_FALSE, /* endian_little */						\
-  CL_TRUE, /* available */						\
-  CL_TRUE, /* compiler_available */					\
-  CL_EXEC_KERNEL, /*execution_capabilities */				\
-  CL_QUEUE_PROFILING_ENABLE, /* queue_properties */			\
-  0, /* platform */							\
-  {0}, /* device_partition_properties */ \
-  0, /* printf_buffer_size */						\
-  "cellspu", /* short_name */							\
-  0, /* long_name */							\
-  "STI", /* vendor */							\
-  PACKAGE_VERSION, /* driver_version */						\
-  "EMBEDDED_PROFILE", /* profile */						\
-  "OpenCL 1.2 pocl", /* version */					\
-  "", /* extensions */							\
+  .preferred_vector_width_char = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_CHAR  ,    \
+  .preferred_vector_width_short = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_SHORT ,   \
+  .preferred_vector_width_int = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_INT   ,     \
+  .preferred_vector_width_long = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_LONG  ,    \
+  .preferred_vector_width_float = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_FLOAT ,   \
+  .preferred_vector_width_double = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_DOUBLE,  \
+  .preferred_vector_width_half = POCL_DEVICES_PREFERRED_VECTOR_WIDTH_HALF  ,    \
+  .max_clock_frequency = 100, 						\
+  .address_bits = 0, 							\
+  .max_mem_alloc_size = 0, 						\
+  .image_support = CL_FALSE, 						\
+  .single_fp_config = CL_FP_ROUND_TO_NEAREST | CL_FP_INF_NAN, 	        \
+  .double_fp_config = CL_FP_ROUND_TO_NEAREST | CL_FP_INF_NAN,           \
+  .global_mem_cache_type = CL_NONE, 					\
+  .local_mem_type = CL_GLOBAL, 					        \
+  .error_correction_support = CL_FALSE, 				\
+  .host_unified_memory = CL_TRUE,                                       \
+  .endian_little = CL_FALSE, 						\
+  .available = CL_TRUE, 						\
+  .compiler_available = CL_TRUE, 					\
+  .execution_capabilities = CL_EXEC_KERNEL, 				\
+  .queue_properties = CL_QUEUE_PROFILING_ENABLE, 			\
+  .short_name = "cellspu", 					        \
+  .vendor = "STI", 						        \
+  .profile = "EMBEDDED_PROFILE",                                        \
+  .extensions = "", 							\
   /* implementation */							\
-  pocl_cellspu_uninit, /* init */                                     \
-  pocl_cellspu_init, /* init */                                       \
-  pocl_cellspu_malloc, /* malloc */					\
-  pocl_cellspu_create_sub_buffer, \
-  pocl_cellspu_free, /* free */						\
-  pocl_cellspu_read, /* read */						\
-  pocl_cellspu_read_rect, /* read_rect */				\
-  pocl_cellspu_write, /* write */					\
-  pocl_cellspu_write_rect, /* write_rect */				\
-  pocl_cellspu_copy, /* copy */						\
-  pocl_cellspu_copy_rect, /* copy_rect */				\
-  NULL, /* fill_rect */							\
-  pocl_cellspu_map_mem,                               \
-  NULL, /* unmap_mem is a NOP */                    \
-  pocl_cellspu_run, /* run */                         \
-  NULL, /* run_native */						\
-  pocl_cellspu_get_timer_value,                \
-  NULL, /*pocl_cellspu_build_program */	 \
-  NULL, /* init_build */ \
-  NULL, /* get_supported_image_formats */ \
-  NULL, /* data */                               \
-  "cellspu-v0", /* llvm_target_triplet */               \
-  "cellspu",  /* llvm_cpu */ \
-  0, /* dev_id */ \
-  0  /* has_64bit_long */ \
+  .uninit = pocl_cellspu_uninit,                                        \
+  .init = pocl_cellspu_init,                                            \
+  .malloc = pocl_cellspu_malloc, 					\
+  .create_sub_buffer = pocl_cellspu_create_sub_buffer,                  \
+  .free = pocl_cellspu_free, 						\
+  .read = pocl_cellspu_read, 						\
+  .read_rect = pocl_cellspu_read_rect, 				        \
+  .write = pocl_cellspu_write, 					        \
+  .write_rect = pocl_cellspu_write_rect, 				\
+  .copy = pocl_cellspu_copy, 						\
+  .copy_rect = pocl_cellspu_copy_rect, 				        \
+  .map_mem = pocl_cellspu_map_mem,                                      \
+  .run = pocl_cellspu_run,                                              \
+  .get_timer_value = pocl_cellspu_get_timer_value,                      \
+  .llvm_target_triplet = "cellspu-v0",                                  \
+  .llvm_cpu = "cellspu",                                                \
 }
 
 #ifdef __cplusplus
