77bf491c2469f325e87330e84c5be0218f86ada7
Kalle Raiskila <kraiskil@ovi.com>
2014-02-17 14:23:28 +0200

    Silence overly pedantic compiler.

--- lib/CL/pocl_llvm.c
+++ lib/CL/pocl_llvm.c
@@ -238,10 +238,13 @@ void pocl_llvm_update_binaries (cl_program program) {
        data updated to the program object in clBuildProgram. */
 }
 
-int get_kernel_names( cl_program, const char **, int )
+int get_kernel_names( cl_program prog, const char **kernels, int num )
 {
   /* implementation only done in API mode */
+  (void)prog;
+  (void)kernels;
+  (void)num;
   POCL_ABORT_UNIMPLEMENTED();
-  return NULL;
+  return 0;
 }
 #endif
