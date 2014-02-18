fd28ef93f9467575737aae30df9e71d2bccd289d
Kalle Raiskila <kraiskil@ovi.com>
2014-02-18 18:06:00 +0200

    buildbot scripts: add LLVM builder, run file through beautifier.

--- doc/buildbot/poclfactory.py
+++ doc/buildbot/poclfactory.py
@@ -1,4 +1,8 @@
-# vim: set noexpandtab:
+#
+# Buildbot scripts to create 'buildbot factories' for pocl and LLVM.
+# Author: Kalle Raiskila, no rights reserved.
+#
+
 from buildbot.process import factory
 from buildbot.steps import source
 from buildbot.steps.shell import ShellCommand, Compile
@@ -6,10 +10,10 @@ import os
 
 #The names of the external tests sources
 AMD_test_pkg='AMD-APP-SDK-v2.8-RC-lnx64.tgz'
-ViennaCL_test_pkg='ViennaCL-1.3.1-src.tar.gz'
+ViennaCL_test_pkg='ViennaCL-1.5.1-src.tar.gz'
 
 
-def createPoclFactory(	environ={}, 
+def createPoclFactory(	environ={},
 			repository='https://github.com/pocl/pocl.git',
 			branch='master',
 			buildICD=True,
@@ -23,7 +27,7 @@ def createPoclFactory(	environ={},
 	Create a buildbot factory object that builds pocl.
 	
 	environ		Dictionary:   The environment variables to append to the build. PATH and
-				LD_LIBRARY_PATH will be added from llvm_dir (if given). 
+				LD_LIBRARY_PATH will be added from llvm_dir (if given).
 	repository	String: the repo to build from. defaults to pocl on github
 	branch		String: the branch in 'repository' to build from. default to master
 	buildICD	Bool:	if false, the ICD extension is not built.
@@ -38,12 +42,12 @@ def createPoclFactory(	environ={},
 	"""
 
 	environ['PATH'] = llvm_dir+"/bin/:${PATH}"
-	environ['LD_LIBRARY_PATH'] = llvm_dir+"/lib/:${LD_LIBRARY_PATH}"	
+	environ['LD_LIBRARY_PATH'] = llvm_dir+"/lib/:${LD_LIBRARY_PATH}"
 
 
 
 	f = factory.BuildFactory()
-	f.addStep(source.Git( 
+	f.addStep(source.Git(
 			repourl=repository,
 			#mode='update',
 			#rm -rf the build tree. Have this only when changing
@@ -52,7 +56,7 @@ def createPoclFactory(	environ={},
 			branch=branch ))
 
 
-        f.addStep(ShellCommand(command=["./autogen.sh"],
+	f.addStep(ShellCommand(command=["./autogen.sh"],
 				haltOnFailure=True,
 				name="autoconfig",
 				env=environ,
@@ -63,7 +67,7 @@ def createPoclFactory(	environ={},
 	if os.path.exists(tests_dir+AMD_test_pkg):
 		f.addStep(ShellCommand(
 				haltOnFailure=True,
-				command=["cp", "-u", tests_dir+AMD_test_pkg, 
+				command=["cp", "-u", tests_dir+AMD_test_pkg,
 					 "examples/AMD/"+AMD_test_pkg],
 				name="copy AMD",
 				description="copying",
@@ -84,33 +88,33 @@ def createPoclFactory(	environ={},
 	if buildICD==False:
 		configOpts = configOpts + ['--disable-icd']
 
-	f.addStep(ShellCommand(command=["./configure"] + configOpts, 
+	f.addStep(ShellCommand(command=["./configure"] + configOpts,
 				haltOnFailure=True,
 				name="configure",
 				env=environ,
-				description="configureing", 
+				description="configureing",
 				descriptionDone="configure"))
-        f.addStep(Compile( 
+	f.addStep(Compile(
 			env=environ ))
 	
 	#enable this later
 	ttacheck=False
 	if ttacheck:
-	        f.addStep(ShellCommand(command=["./tools/scripts/run_tta_tests"],
+		f.addStep(ShellCommand(command=["./tools/scripts/run_tta_tests"],
 				haltOnFailure=True,
 				name="checks",
 				env=environ,
-				description="testing", 
+				description="testing",
 				descriptionDone="tests",
 				logfiles={"test.log": "tests/testsuite.log"},
 				timeout=60*60))
 	else:
-	        f.addStep(ShellCommand(command=["make", "check"],
+		f.addStep(ShellCommand(command=["make", "check"],
 				#for beagle, contiunu to clean it up
 				#haltOnFailure=True,
 				name="checks",
 				env=environ,
-				description="testing", 
+				description="testing",
 				descriptionDone="tests",
 				logfiles={"test.log": "tests/testsuite.log"},
 				#blas3 alone takes 15-20 min.
@@ -125,21 +129,82 @@ def createPoclFactory(	environ={},
 	#		ShellCommand(
 	#			haltOnFailure=True,
 	#			env=environ,
-	#			command=['./tools/scripts/benchmark.py', '--lightweight', '-o', 'benchmark_log.txt' ], 
+	#			command=['./tools/scripts/benchmark.py', '--lightweight', '-o', 'benchmark_log.txt' ],
 	#			logfiles = {'log.txt': 'benchmark_log.txt'},
 	#			name = 'benchmark',
-	#			description='benchmarking', 
+	#			description='benchmarking',
 	#			descriptionDone='benchmarked',
 	#			# 4hour timeout - PPC runs for a *long* time
 	#			timeout=60*60*4))
 	#	f.addStep(
 	#		ShellCommand(
 	#			command=[
-	#				'scp', 
+	#				'scp',
 	#				'benchmark_log.txt',
-	#				WithProperties("marvin:/var/www/pocl_benchmarks/benchmark-"+processor+"-r%(got_revision)s.txt")], 
+	#				WithProperties("marvin:/var/www/pocl_benchmarks/benchmark-"+processor+"-r%(got_revision)s.txt")],
 	#			name = 'copy benchmark',
-	#			description='copying', 
+	#			description='copying',
 	#			descriptionDone='copied'))
 
 	return f
+
+#######
+## LLVM/clang builder
+##
+
+def createLLVMFactory(srcdir, branch, builddir, installdir):
+	f = factory.BuildFactory()
+	f.addStep(
+		SVN(
+			name='svn-llvm',
+			mode='update',
+			baseURL='http://llvm.org/svn/llvm-project/llvm/',
+			defaultBranch=branch,
+			workdir=srcdir))
+	f.addStep(
+		SVN(
+			name='svn-clang',
+			mode='update',
+			baseURL='http://llvm.org/svn/llvm-project/cfe/',
+			defaultBranch=branch,
+			workdir='%s/tools/clang' % srcdir))
+	f.addStep(
+		ShellCommand(
+			command=[
+				'%s/configure' % srcdir,
+				'--prefix=' + installdir,
+				'--enable-targets=host',
+				'--enable-shared', '--disable-static'],
+			workdir=builddir,
+			haltOnFailure=True,
+			name="configure",
+			descriptionDone='configure',
+			description='configuring'))
+	f.addStep(
+		ShellCommand(
+			command=['make'],
+			workdir=builddir,
+			haltOnFailure=True,
+			name = "compile",
+			descriptionDone = 'compile',
+			description='compiling'))
+	f.addStep(
+		ShellCommand(
+			command=['make', 'check'],
+			workdir=builddir,
+			name='check',
+			descriptionDone='check',
+			haltOnFailure=True,
+			description='checking'))
+	f.addStep(
+		ShellCommand(
+			command=['make', 'install'],
+			workdir=builddir,
+			haltOnFailure=True,
+			name = 'install',
+			descriptionDone='install',
+			description='installing'))
+
+	return f
+
+# vim: set noexpandtab:
