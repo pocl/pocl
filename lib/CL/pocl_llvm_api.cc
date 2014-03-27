8c7de3721c87dabfc0cf3458184707c66a19e873
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2014-03-27 17:41:18 +0200

    program.bc was not dumped when instructed with POCL_LEAVE_TEMP_DIRS.

--- lib/CL/pocl_llvm_api.cc
+++ lib/CL/pocl_llvm_api.cc
@@ -350,7 +350,7 @@ int pocl_llvm_build_program(cl_program program,
     delete (llvm::Module*)*mod;
   *mod = action->takeModule();
 
-  if (!pocl_get_bool_option("POCL_LEAVE_TEMP_DIRS", 0))
+  if (pocl_get_bool_option("POCL_LEAVE_TEMP_DIRS", 0))
     write_temporary_file(*mod, binary_file_name);
 
   // FIXME: cannot delete action as it contains something the llvm::Module
