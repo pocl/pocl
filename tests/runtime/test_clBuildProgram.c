cfc811f3ef4f4ad72a107ae2e39f89a6d33a1fb7
Michal Babej <Franz.Netykafka@runbox.com>
2014-07-01 10:19:38 +0200

    Add tests for commit c94ed021030bbc85a6c5d281e5a73c362cfdd871 ..
    ... "Properly handle kernel compilation failure" - check for
    CL_BUILD_PROGRAM_FAILURE of invalid kernels

--- tests/runtime/test_clBuildProgram.c
+++ tests/runtime/test_clBuildProgram.c
@@ -26,6 +26,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <CL/cl.h>
+#include <assert.h>
 
 #define MAX_PLATFORMS 32
 #define MAX_DEVICES   32
@@ -36,6 +37,9 @@
 char kernel[] = 
   "#include \"test_kernel_src_in_another_dir.h\"\n"
   "#include \"test_kernel_src_in_pwd.h\"\n";
+
+char invalid_kernel[] =
+  "kernel void test_kernel(constant int a, j) { return 3; }\n";
 
 int
 main(void){
@@ -64,16 +68,27 @@ main(void){
   char* kernel_buffer = kernel;
 
   program = clCreateProgramWithSource(context, 1, (const char**)&kernel_buffer, 
-				      &kernel_size, &err);
-  if (err != CL_SUCCESS)
-    return EXIT_FAILURE;
+                                     &kernel_size, &err);
+  assert((err == CL_SUCCESS) && "clCreateProgramWithSource for the kernel with #include failed");
 
   err = clBuildProgram
     (program, num_devices, devices, 
      "-D__FUNC__=helper_func -I./test_data", 
      NULL, NULL);
-  if (err != CL_SUCCESS)
-    return EXIT_FAILURE;
+  assert((err == CL_SUCCESS) && "clBuildProgram failed");
 
-  return err == CL_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+  err = clReleaseProgram(program);
+  assert((err == CL_SUCCESS) && "clReleaseProgram failed");
+
+  kernel_size = strlen(invalid_kernel);
+  kernel_buffer = invalid_kernel;
+
+  program = clCreateProgramWithSource(context, 1, (const char**)&kernel_buffer,
+				      &kernel_size, &err);
+  assert((err == CL_SUCCESS) && "clCreateProgramWithSource for invalid kernel failed");
+
+  err = clBuildProgram(program, num_devices, devices, NULL, NULL, NULL);
+  assert((err == CL_BUILD_PROGRAM_FAILURE) && "Compilation of invalid kernels did not fail with CL_BUILD_PROGRAM_FAILURE");
+
+  return EXIT_SUCCESS;
 }
