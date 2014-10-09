5dd8464bc6bec40c4023f764fceff78318622b5f
Michal Babej <Franz.Netykafka@runbox.com>
2014-10-09 19:07:18 +0200

    Add abort message for clEnqueueWaitForEvents

--- lib/CL/clEnqueueWaitForEvents.c
+++ lib/CL/clEnqueueWaitForEvents.c
@@ -5,7 +5,7 @@ POname(clEnqueueWaitForEvents)(cl_command_queue  command_queue,
                        const cl_event *  event_list) 
 CL_API_SUFFIX__VERSION_1_0
 {
-  POCL_ABORT_UNIMPLEMENTED();
+  POCL_ABORT_UNIMPLEMENTED("The entire clEnqueueWaitForEvents call");
   return CL_SUCCESS;
 }
 POsym(clEnqueueWaitForEvents)
