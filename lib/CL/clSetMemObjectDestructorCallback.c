cd387cbd4a4dcbc05e02c908571e27285d51796d
Michal Babej <Franz.Netykafka@runbox.com>
2014-10-10 14:28:28 +0200

    Add abort messages to clSetUserEventStatus and clSetMemObjectDestructorCallback

--- lib/CL/clSetMemObjectDestructorCallback.c
+++ lib/CL/clSetMemObjectDestructorCallback.c
@@ -5,7 +5,7 @@ POname(clSetMemObjectDestructorCallback)(  cl_mem  memobj ,
                                     void (CL_CALLBACK * pfn_notify)( cl_mem /* memobj */, void* /*user_data*/), 
                                     void * user_data  )             CL_API_SUFFIX__VERSION_1_1
 {
-  POCL_ABORT_UNIMPLEMENTED();
+  POCL_ABORT_UNIMPLEMENTED("The entire clSetMemObjectDestructorCallback call");
   return CL_SUCCESS;
 }
 POsym(clSetMemObjectDestructorCallback)
--- lib/CL/clSetUserEventStatus.c
+++ lib/CL/clSetUserEventStatus.c
@@ -3,7 +3,7 @@ CL_API_ENTRY cl_int CL_API_CALL
 POname(clSetUserEventStatus)(cl_event    event ,
                      cl_int      execution_status ) CL_API_SUFFIX__VERSION_1_1
 {
-  POCL_ABORT_UNIMPLEMENTED();
+  POCL_ABORT_UNIMPLEMENTED("The entire clSetUserEventStatus call");
   return CL_SUCCESS;
 }
 POsym(clSetUserEventStatus)
