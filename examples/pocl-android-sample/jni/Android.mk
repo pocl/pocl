c093225faa4da9b473add1dbd20fd4e2cec1a9bd
Krishnaraj R Bhat <krrishnarraj@gmail.com>
2014-12-27 13:41:27 +0530

    Link to libopencl-stub statically instead of directly including its sources in pocl-android-example

--- examples/pocl-android-sample/jni/Android.mk
+++ examples/pocl-android-sample/jni/Android.mk
@@ -1,13 +1,12 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_LDLIBS := -ldl -llog
 LOCAL_MODULE := poclVecAdd
-
-LOCAL_C_INCLUDES +=                       \
-    $(LOCAL_PATH)/libopencl-stub/include/
-
-LOCAL_SRC_FILES :=              \
-    vectorAdd.cpp               \
-    libopencl-stub/libopencl.c
-
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/libopencl-stub/include/
+LOCAL_SRC_FILES := vectorAdd.cpp
+LOCAL_CFLAGS   = -fPIC -O2
+LOCAL_STATIC_LIBRARIES := OpenCL
+LOCAL_LDLIBS := -ldl -llog
 include $(BUILD_SHARED_LIBRARY)
+
+include $(LOCAL_PATH)/libopencl-stub/Android.mk
