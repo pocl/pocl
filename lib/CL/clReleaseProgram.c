10ad70accf6cd0ce73d815d9afac7520fd03aa5a
Michal Babej <Franz.Netykafka@runbox.com>
2015-03-27 16:03:42 +0100

    clReleaseProgram.c: move to new kernel cache functions

--- lib/CL/clReleaseProgram.c
+++ lib/CL/clReleaseProgram.c
@@ -32,7 +32,7 @@
 
 #include "pocl_cl.h"
 #include "pocl_util.h"
-#include "pocl_runtime_config.h"
+#include "pocl_cache.h"
 
 CL_API_ENTRY cl_int CL_API_CALL
 POname(clReleaseProgram)(cl_program program) CL_API_SUFFIX__VERSION_1_0
@@ -61,7 +61,7 @@ POname(clReleaseProgram)(cl_program program) CL_API_SUFFIX__VERSION_1_0
 
       POCL_RELEASE_OBJECT (program->context, new_refcount);
       POCL_MEM_FREE(program->source);
-      
+
       if (program->binaries != NULL)
         {
           POCL_MEM_FREE(program->binaries[0]);
@@ -69,15 +69,9 @@ POname(clReleaseProgram)(cl_program program) CL_API_SUFFIX__VERSION_1_0
         }
       POCL_MEM_FREE(program->binary_sizes);
 
-      if ((!pocl_get_bool_option("POCL_KERNEL_CACHE", POCL_BUILD_KERNEL_CACHE)) &&
-            (!pocl_get_bool_option("POCL_LEAVE_KERNEL_COMPILER_TEMP_FILES", 0)) &&
-            program->cache_dir)
-        {
-          pocl_rm_rf (program->cache_dir);
-        }
+      pocl_cache_cleanup_cachedir(program);
 
       POCL_MEM_FREE(program->llvm_irs);
-      POCL_MEM_FREE(program->cache_dir);
       POCL_MEM_FREE(program);
     }
 
