13e5bc89a6b7675efbc786e5e191b2d98db05b01
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2015-04-01 14:52:27 +0200

    Test for linking errors during clBuildProgram
    clBuildProgram should fail if linking is not possible (e.g. due to an
    unresolved symbol, missing function definition, or whatever else).
    
    The test currently fails (i.e. building “succeeds”) due to the delayed
    linking in pocl.

---
+++ tests/runtime/test_link_error.c
@@ -0,0 +1,75 @@
+/* Test that link errors are caught at clBuildProgram time
+
+   Copyright (c) 2015 Giuseppe Bilotta
+                      Kalray
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <CL/cl.h>
+#include <poclu.h>
+#include "pocl_tests.h"
+
+#define MAX_PLATFORMS 32
+#define MAX_DEVICES   32
+#define MAX_BINARIES  32
+
+/* A program that fails to link */
+char program_src[] =
+  "void test_func();\n" /* declared but not defined */
+  "kernel void test_kernel(global int *a) { test_func(); }\n";
+
+int
+main(void){
+  cl_int err;
+  cl_platform_id platforms[MAX_PLATFORMS];
+  cl_uint nplatforms;
+  cl_device_id devices[MAX_DEVICES + 1]; // + 1 for duplicate test
+  cl_uint num_devices;
+  cl_uint i;
+  cl_program program = NULL;
+  cl_program program_with_binary = NULL;
+  err = clGetPlatformIDs(MAX_PLATFORMS, platforms, &nplatforms);
+  CHECK_OPENCL_ERROR_IN("clGetPlatformIDs");
+  if (!nplatforms)
+    return EXIT_FAILURE;
+
+  err = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_ALL, MAX_DEVICES,
+		       devices, &num_devices);  
+  CHECK_OPENCL_ERROR_IN("clGetDeviceIDs");
+
+  cl_context context = clCreateContext(NULL, num_devices, devices, NULL, NULL, &err);
+  CHECK_OPENCL_ERROR_IN("clCreateContext");
+
+  size_t program_size = strlen(program_src);
+  char* program_buffer = program_src;
+
+  program = clCreateProgramWithSource(context, 1, (const char**)&program_buffer, 
+                                     &program_size, &err);
+  //clCreateProgramWithSource for the program with #include failed
+  CHECK_OPENCL_ERROR_IN("clCreateProgramWithSource");
+
+  err = clBuildProgram(program, num_devices, devices, NULL, NULL, NULL);
+  TEST_ASSERT(err == CL_BUILD_PROGRAM_FAILURE);
+
+  return EXIT_SUCCESS;
+}
