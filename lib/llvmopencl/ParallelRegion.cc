pocl/pocl

commit 10793bfdeba45def712ca3ee1a46c8f7feaa86a6

Track LLVM 3.7 API change.

lib/llvmopencl/ParallelRegion.cc
@@ -741,7 +741,7 @@ ParallelRegion::InjectPrintF
   assert (isa<Constant>(stringArg));
   Constant* const_ptr_8 = 
     ConstantExpr::getGetElementPtr
-    (cast<Constant>(stringArg), const_ptr_8_indices);
+    (PointerType::getUnqual(Type::getInt8Ty(getGlobalContext())), cast<Constant>(stringArg), const_ptr_8_indices);
 
   std::vector<Value*> args;
   args.push_back(const_ptr_8);

commit c8ae7845742315e754896f92ddf7392080537059

Fix build against <LLVM 3.7, broken in 10793bfd.

lib/llvmopencl/ParallelRegion.cc
@@ -739,9 +739,15 @@ ParallelRegion::InjectPrintF
   const_ptr_8_indices.push_back(const_int64_9);
   const_ptr_8_indices.push_back(const_int64_9);
   assert (isa<Constant>(stringArg));
+  #ifdef LLVM_OLDER_THAN_3_7
+  Constant* const_ptr_8 = 
+    ConstantExpr::getGetElementPtr
+    (cast<Constant>(stringArg), const_ptr_8_indices);
+  #else
   Constant* const_ptr_8 = 
     ConstantExpr::getGetElementPtr
     (PointerType::getUnqual(Type::getInt8Ty(getGlobalContext())), cast<Constant>(stringArg), const_ptr_8_indices);
+  #endif
 
   std::vector<Value*> args;
   args.push_back(const_ptr_8);