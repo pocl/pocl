c6a23d47c16080ce7b221d09d7da2ebef4f73e8a
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2015-04-06 20:04:35 +0300

    AMD ICD loader supports OPENCL_VENDOR_PATH for overriding the ICD dir. Set it also so AMD ICD loader can be used with the pocl test suite.

--- tools/scripts/devel-envs.sh
+++ tools/scripts/devel-envs.sh
@@ -2,6 +2,9 @@
 export POCL_BUILDING=1
 export OCL_ICD_VENDORS=$PWD/ocl-vendors
 
+# AMDSDK supports the overriding via other env name.
+export OPENCL_VENDOR_PATH=$OCL_ICD_VENDORS
+
 #pocl test-cases don't link against pthreads, but libpocl does.
 #this confuses gdb, unless we preload libpthread
 #If libpocl is not built yet, this will fail...
