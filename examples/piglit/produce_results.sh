4f565767630730785aca046075aa5d696beb92a0
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2015-04-12 15:02:39 +0300

    Filter out an interministically passing piglit test case.

--- examples/piglit/produce_results.sh
+++ examples/piglit/produce_results.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-piglit/piglit-run.py -v piglit/tests/cl.tests piglit/results/all 2>&1 | egrep "^pass:" > result
+# The test case seems to pass sometimes indeterministically. Filter it out
+# to make regression checking feasible.
+piglit/piglit-run.py -v piglit/tests/cl.tests piglit/results/all 2>&1 | egrep "^pass:" | \
+egrep -v "program@execute@builtin@builtin-float-asinh-1.0.generated" > result
 sed -i "s/[ \t]*$//" result
 LC_ALL=C sort result -o sorted_result
