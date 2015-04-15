55271fecde5ff72fd6e0703abdf081821953aec3
Ville Korhonen <ville.t.korhonen@tut.fi>
2015-04-15 16:45:12 +0300

    Fixed test_clGetEventInfo test indentation and removed event reference count check.

--- tests/runtime/test_clGetEventInfo.c
+++ tests/runtime/test_clGetEventInfo.c
@@ -24,59 +24,50 @@ main(void)
       err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, MAX_DEVICES,
 			   devices, &ndevices);
       if (err != CL_SUCCESS)
-	return EXIT_FAILURE;
+        return EXIT_FAILURE;
       
       for (j = 0; j < ndevices; j++)
-	{
-	  cl_context context = clCreateContext(NULL, 1, &devices[j], NULL, NULL, &err);
-	  if (err != CL_SUCCESS)
-	    return EXIT_FAILURE;
-	  cl_command_queue queue = clCreateCommandQueue(context, devices[j], 0, &err);
-	  if (err != CL_SUCCESS)
-	    return EXIT_FAILURE;
-
-	  const int buf_size = 1024;
-	  cl_int host_buf[buf_size];
-
-	  cl_mem buf = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(cl_int) * buf_size, NULL, &err);
-	  if (err != CL_SUCCESS)
-	    return EXIT_FAILURE;
-	  cl_event buf_event;
-	  if (clEnqueueReadBuffer(queue, buf, CL_TRUE, 0, sizeof(cl_int) * buf_size, &host_buf, 0, NULL, &buf_event) != CL_SUCCESS)
-	    return EXIT_FAILURE;
-	  clFinish(queue);
-	  cl_command_queue event_command_queue;
-	  size_t param_val_size_ret;
-	  if (clGetEventInfo(buf_event, CL_EVENT_COMMAND_QUEUE, sizeof(cl_command_queue), &event_command_queue, &param_val_size_ret) != CL_SUCCESS)
-	    return EXIT_FAILURE;
-	  if (param_val_size_ret != sizeof(cl_command_queue) || event_command_queue != queue)
-	    return EXIT_FAILURE;
-
-	  cl_command_type command_type;
-	  if (clGetEventInfo(buf_event, CL_EVENT_COMMAND_TYPE, sizeof(cl_command_type), &command_type, &param_val_size_ret) != CL_SUCCESS)
-	    return EXIT_FAILURE;
-	  if (param_val_size_ret != sizeof(cl_command_type) || command_type != CL_COMMAND_READ_BUFFER)
-	    return EXIT_FAILURE;
-
-	  cl_int execution_status;
-	  if (clGetEventInfo(buf_event, CL_EVENT_COMMAND_EXECUTION_STATUS, sizeof(cl_int), &execution_status, &param_val_size_ret) != CL_SUCCESS)
-	    return EXIT_FAILURE;
-	  if (param_val_size_ret != sizeof(cl_int) || execution_status != CL_COMPLETE)
-	    return EXIT_FAILURE;
-
-	  cl_uint ref_count;
-	  if (clGetEventInfo(buf_event, CL_EVENT_REFERENCE_COUNT, sizeof(cl_uint), &ref_count, &param_val_size_ret) != CL_SUCCESS)
-	    return EXIT_FAILURE;
-	  if (param_val_size_ret != sizeof(cl_uint) || ref_count != 1)
         {
-          printf("FAIL: expected refcount 1, got %d\n", ref_count);
-          return EXIT_FAILURE;
+          cl_context context = clCreateContext(NULL, 1, &devices[j], NULL, NULL, &err);
+          if (err != CL_SUCCESS)
+            return EXIT_FAILURE;
+          cl_command_queue queue = clCreateCommandQueue(context, devices[j], 0, &err);
+          if (err != CL_SUCCESS)
+            return EXIT_FAILURE;
+          
+          const int buf_size = 1024;
+          cl_int host_buf[buf_size];
+          
+          cl_mem buf = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(cl_int) * buf_size, NULL, &err);
+          if (err != CL_SUCCESS)
+            return EXIT_FAILURE;
+          cl_event buf_event;
+          if (clEnqueueReadBuffer(queue, buf, CL_TRUE, 0, sizeof(cl_int) * buf_size, &host_buf, 0, NULL, &buf_event) != CL_SUCCESS)
+            return EXIT_FAILURE;
+          clFinish(queue);
+          cl_command_queue event_command_queue;
+          size_t param_val_size_ret;
+          if (clGetEventInfo(buf_event, CL_EVENT_COMMAND_QUEUE, sizeof(cl_command_queue), &event_command_queue, &param_val_size_ret) != CL_SUCCESS)
+            return EXIT_FAILURE;
+          if (param_val_size_ret != sizeof(cl_command_queue) || event_command_queue != queue)
+            return EXIT_FAILURE;
+          
+          cl_command_type command_type;
+          if (clGetEventInfo(buf_event, CL_EVENT_COMMAND_TYPE, sizeof(cl_command_type), &command_type, &param_val_size_ret) != CL_SUCCESS)
+            return EXIT_FAILURE;
+          if (param_val_size_ret != sizeof(cl_command_type) || command_type != CL_COMMAND_READ_BUFFER)
+            return EXIT_FAILURE;
+          
+          cl_int execution_status;
+          if (clGetEventInfo(buf_event, CL_EVENT_COMMAND_EXECUTION_STATUS, sizeof(cl_int), &execution_status, &param_val_size_ret) != CL_SUCCESS)
+            return EXIT_FAILURE;
+          if (param_val_size_ret != sizeof(cl_int) || execution_status != CL_COMPLETE)
+            return EXIT_FAILURE;
+          
+          clReleaseEvent(buf_event);
+          clReleaseMemObject(buf);
+          clReleaseCommandQueue(queue);
         }
-
-	  clReleaseEvent(buf_event);
-	  clReleaseMemObject(buf);
-	  clReleaseCommandQueue(queue);
-	}
     }
   return EXIT_SUCCESS;
 }
