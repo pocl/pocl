bfee547c3233656d831fc847354d52463e7d6f73
Pavan Yalamanchili <pavan@arrayfire.com>
2015-08-04 10:05:42 -0400

    Fixes a bug in clGetDeviceIDs when device type isn't found
    - Now returns CL_DEVICE_NOT_FOUND when no devices found

--- lib/CL/clGetDeviceIDs.c
+++ lib/CL/clGetDeviceIDs.c
@@ -1,17 +1,17 @@
 /* OpenCL runtime library: clGetDeviceIDs()
 
-   Copyright (c) 2011 Kalle Raiskila 
-   
+   Copyright (c) 2011 Kalle Raiskila
+
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
    in the Software without restriction, including without limitation the rights
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
-   
+
    The above copyright notice and this permission notice shall be included in
    all copies or substantial portions of the Software.
-   
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,10 +26,10 @@
 #include <string.h>
 
 CL_API_ENTRY cl_int CL_API_CALL
-POname(clGetDeviceIDs)(cl_platform_id   platform, 
-               cl_device_type   device_type, 
-               cl_uint          num_entries, 
-               cl_device_id *   devices, 
+POname(clGetDeviceIDs)(cl_platform_id   platform,
+               cl_device_type   device_type,
+               cl_uint          num_entries,
+               cl_device_id *   devices,
                cl_uint *        num_devices) CL_API_SUFFIX__VERSION_1_0
 {
   int total_num = 0;
@@ -45,12 +45,15 @@ POname(clGetDeviceIDs)(cl_platform_id   platform,
 
   total_num = pocl_get_device_type_count(device_type);
 
+  if (total_num == 0)
+      return CL_DEVICE_NOT_FOUND;
+
   if (devices != NULL)
     devices_added = pocl_get_devices(device_type, devices, num_entries);
- 
+
   if (num_devices != NULL)
     *num_devices = total_num;
-  
+
   if (devices_added > 0 || num_entries == 0)
     return CL_SUCCESS;
   else
