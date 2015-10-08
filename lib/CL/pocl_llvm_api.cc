6016e90b4d008e93a89f216e7a40b517ec4a53b4
Michal Babej <michal.babej@tut.fi>
2015-10-08 15:38:07 +0200

    Fix the previous commit's fix... a bit too much removing.

--- lib/CL/pocl_llvm_api.cc
+++ lib/CL/pocl_llvm_api.cc
@@ -1556,6 +1556,7 @@ pocl_llvm_codegen(cl_kernel kernel,
 #ifdef LLVM_OLDER_THAN_3_7
     std::string data;
     llvm::raw_string_ostream sos(data);
+    llvm::MCContext *mcc;
     if (target && target->addPassesToEmitMC(PM, mcc, sos))
       return 1;
 #else
