420a9d21eef0e4c38350a14c23fe6a2b5d5860e0
Michal Babej <michal.babej@tut.fi>
2015-11-02 20:23:59 +0100

    pocl.h: add command structs for SVM memory ops

--- include/pocl.h
+++ include/pocl.h
@@ -134,7 +134,6 @@ typedef struct
   mem_mapping_t *mapping;
 } _cl_command_map;
 
-
 /* clEnqueue(Write/Read)Image */
 typedef struct
 {
@@ -173,6 +172,44 @@ typedef struct
   void *data;
 } _cl_command_marker;
 
+typedef struct
+{
+  void* data;
+  cl_uint  num_svm_pointers;
+  void  **svm_pointers;
+  void (CL_CALLBACK  *pfn_free_func) ( cl_command_queue queue,
+                                       cl_uint num_svm_pointers,
+                                       void *svm_pointers[],
+                                       void  *user_data);
+} _cl_command_svm_free;
+
+typedef struct
+{
+  void* svm_ptr;
+  size_t size;
+  cl_map_flags flags;
+} _cl_command_svm_map;
+
+typedef struct
+{
+  void* svm_ptr;
+} _cl_command_svm_unmap;
+
+typedef struct
+{
+  void* svm_ptr;
+  size_t size;
+  void* pattern;
+  size_t pattern_size;
+} _cl_command_svm_fill;
+
+typedef struct
+{
+  const void* src;
+  void* dst;
+  size_t size;
+} _cl_command_svm_cpy;
+
 typedef union
 {
   _cl_command_run run;
@@ -185,6 +222,11 @@ typedef union
   _cl_command_rw_image rw_image;
   _cl_command_marker marker;
   _cl_command_unmap unmap;
+  _cl_command_svm_free svm_free;
+  _cl_command_svm_map svm_map;
+  _cl_command_svm_unmap svm_unmap;
+  _cl_command_svm_fill svm_memfill;
+  _cl_command_svm_cpy svm_memcpy;
 } _cl_command_t;
 
 // one item in the command queue
