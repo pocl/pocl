9764f4d880287c6dd5f4bb1956415816471d7a7a
Michal Babej <michal.babej@tut.fi>
2015-12-07 11:47:52 +0100

    Make the output more readable when the output is a terminal

--- lib/CL/pocl_debug.c
+++ lib/CL/pocl_debug.c
@@ -2,6 +2,7 @@
 
 #ifdef POCL_DEBUG_MESSAGES
 int pocl_debug_messages;
+int stderr_is_a_tty;
 
 #ifdef HAVE_CLOCK_GETTIME
 
@@ -18,9 +19,15 @@ int pocl_debug_messages;
         clock_gettime(CLOCK_REALTIME, &timespec);
         tm_nanosec = timespec.tv_nsec;
         gmtime_r(&timespec.tv_sec, &t);
+        const char* formatstring;
+        if (stderr_is_a_tty)
+          formatstring = BLUE "[%04i-%02i-%02i %02i:%02i:%02i.%09li] "
+              RESET "POCL: in fn" CYAN " %s " RESET "at line %u:\n";
+        else
+          formatstring = "[%04i-%02i-%02i %02i:%02i:%02i.%09li] "
+              "POCL: in fn %s at line %u:\n";
         fprintf(stderr,
-            "[%04i-%02i-%02i %02i:%02i:%02i.%09li] POCL: "
-            "in fn %s at line %u:\n", (t.tm_year + 1900),
+            formatstring, (t.tm_year + 1900),
             t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min,
             t.tm_sec, tm_nanosec,  func, line);
     }
@@ -31,6 +38,49 @@ int pocl_debug_messages;
       clock_gettime(CLOCK_MONOTONIC_RAW, ((struct timespec *)start));
     }
 
+    void pocl_debug_print_duration(const char* func, unsigned line,
+                                   const char* msg, uint64_t nanosecs)
+    {
+      if (!pocl_debug_messages)
+        return;
+      const char* formatstring;
+      if (stderr_is_a_tty)
+        formatstring = "      >>>  " MAGENTA "     %3" PRIu64
+                       ".%03" PRIu64 " " RESET " %s    %s\n";
+      else
+        formatstring = "      >>>       %3" PRIu64 ".%03"
+                       PRIu64 "  %s    %s\n";
+
+      uint64_t nsec = nanosecs % 1000000000;
+      uint64_t sec = nanosecs / 1000000000;
+      uint64_t a, b;
+
+      if ((sec == 0) && (nsec < 1000))
+        {
+          b = nsec % 1000;
+          if (stderr_is_a_tty)
+            formatstring = "      >>>      " MAGENTA "     %3" PRIu64 " " RESET " ns    %s\n";
+          else
+            formatstring = "      >>>           %3" PRIu64 "  ns    %s\n";
+          POCL_MSG_PRINT2(func, line, formatstring, b, msg);
+        }
+      else if ((sec == 0) && (nsec < 1000000))
+        {
+          a = nsec / 1000;
+          b = nsec % 1000;
+          POCL_MSG_PRINT2(func, line, formatstring, a, b, "us", msg);
+        }
+      else if (sec == 0)
+        {
+          a = nsec / 1000000;
+          b = (nsec % 1000000) / 1000;
+          POCL_MSG_PRINT2(func, line, formatstring, a, b, "ms", msg);
+        }
+      else
+          POCL_MSG_PRINT2(func, line, formatstring, sec, nsec, "s", msg);
+
+    }
+
     void pocl_debug_measure_finish(void *start, void *finish,
                                    const char* msg,
                                    const char* func,
@@ -38,17 +88,11 @@ int pocl_debug_messages;
       if (!pocl_debug_messages)
         return;
       clock_gettime(CLOCK_MONOTONIC_RAW, ((struct timespec *)finish));
-      long nsec =  ( ((struct timespec *)finish)->tv_nsec
+      uint64_t nsec =  ( ((struct timespec *)finish)->tv_nsec
                      - ((struct timespec *)start)->tv_nsec);
-      long sec = 0;
-      if (nsec < 0)
-        {
-          sec--;
-          nsec += 1000000000;
-        }
-      sec += ( ((struct timespec *)finish)->tv_sec
+      uint64_t sec = ( ((struct timespec *)finish)->tv_sec
                - ((struct timespec *)start)->tv_sec);
-      POCL_MSG_PRINT2(func, line, "      >>>   %02li.%09li s     %s\n", sec, nsec, msg);
+      pocl_debug_print_duration(func, line, msg, (nsec + (sec * 1000000000)) );
     }
 
   #else
