725c9320d906025de4bcad926d389fa7dbc038cf
Michal Babej <michal.babej@tut.fi>
2016-02-04 18:49:04 +0100

    CMake: Fix pocl-standalone for new kernel lib variants

--- scripts/pocl-standalone.in.cmake
+++ scripts/pocl-standalone.in.cmake
@@ -30,8 +30,6 @@ if [ -n "$POCL_VERBOSE" ]; then
     echo 0=$0 @=$@
 fi
 
-target=@OCL_KERNEL_TARGET@
-
 while getopts h:t:o: o
 do
     case "$o" in
@@ -54,17 +52,18 @@ then
 fi
 
 case $target in
-    tce*)     target_dir="tce"
-               target="tce-tut-llvm"
-               ;;
-    *)         target_dir="host";;
-esac
-
-case $target in
-    tce*)     CLANG_FLAGS="@TCE_TARGET_CLANG_FLAGS@"
+    tce*)     target="tce-tut-llvm"
+              target_dir="tce"
+              KERNLIB="kernel-${target}.bc"
+              CLANG_FLAGS="@TCE_TARGET_CLANG_FLAGS@"
               LLC_FLAGS="@TCE_TARGET_LLC_FLAGS@"
-              LD_FLAGS="@@";;
-    *)        CLANG_FLAGS="@HOST_CLANG_FLAGS@"
+              LD_FLAGS="@@"
+              ;;
+    *)        target=@OCL_KERNEL_TARGET@
+              target_dir="host"
+              cpu=@KERNLIB_VARIANT@
+              KERNLIB="kernel-${target}-${cpu}.bc"
+              CLANG_FLAGS="@HOST_CLANG_FLAGS@"
               DEVICE_CL_FLAGS="@HOST_DEVICE_EXTENSION_DEFINES@"
               LLC_FLAGS="@HOST_LLC_FLAGS@"
               LD_FLAGS="@HOST_LD_FLAGS@";;
@@ -74,7 +73,7 @@ case $target in
 #              LD_FLAGS="@TARGET_LD_FLAGS@";;
 esac
 CLANG_FLAGS="$CLANG_FLAGS -fasm -fsigned-char -Xclang -ffake-address-space-map"
-echo $target
+
 # With fp-contract we get calls to fma with processors which do not
 # have fma instructions. These ruin the performance. Better to have
 # the mul+add separated in the IR.
@@ -88,13 +87,13 @@ kernel_bc="${tempdir}/kernel.bc"
 pocl_kernel_compiler_lib=@LLVMOPENCL_LOCATION@
 @CLANG@ ${CLANG_FLAGS} ${DEVICE_CL_FLAGS} $EXTRA_CLANG_FLAGS -c -emit-llvm @ADD_INCLUDE@ -include @KERNEL_INCLUDE_DIR@/_kernel.h -o ${kernel_bc} -x cl $1
 rm -f ${header}
-@LLVM_OPT@ ${LLC_FLAGS} -load=$pocl_kernel_compiler_lib -generate-header -disable-output -header=${header} ${kernel_bc}
+@LLVM_OPT@ ${LLC_FLAGS} -load=${pocl_kernel_compiler_lib} -generate-header -disable-output -header=${header} ${kernel_bc}
 
 linked_bc="${tempdir}/linked.bc"
 linked_out="${linked_bc}.out"
-full_target_dir=@FULL_TARGET_DIR@
 
-@LLVM_LINK@ -o ${linked_bc} ${kernel_bc} $full_target_dir/kernel-$target.bc
+
+@LLVM_LINK@ -o ${linked_bc} ${kernel_bc} @FULL_TARGET_DIR@/${KERNLIB}
 
 OPT_SWITCH="-O3"
 
