97e7d7f7b46c9d00711a4cd6047d602fead90b1a
Michal Babej <michal.babej@tut.fi>
2016-02-26 01:41:09 +0100

    pocl_cache.c: fix a few small bugs

--- lib/CL/pocl_cache.c
+++ lib/CL/pocl_cache.c
@@ -126,6 +126,7 @@ void pocl_cache_kernel_cachedir(char* kernel_cachedir_path, cl_program   program
   char tempstring[POCL_FILENAME_LENGTH];
   bytes_written = snprintf(tempstring, POCL_FILENAME_LENGTH,
                            "/%s", kernel->name);
+  assert(bytes_written > 0 && bytes_written < POCL_FILENAME_LENGTH);
   program_device_dir(kernel_cachedir_path, program, device_i, tempstring);
 }
 
@@ -389,7 +390,8 @@ build_program_compute_hash(cl_program program,
         pocl_SHA1_Update(&hash_ctx, (uint8_t*)preprocessed_source,
                          source_len);
     } else     { /* Program was created with clCreateProgramWithBinary() */
-        assert(program->binaries[device_i]);
+        assert(program->binaries[device_i] ||
+               program->pocl_binaries[device_i]);
         pocl_SHA1_Update(&hash_ctx,
                          (uint8_t*) program->binaries[device_i],
                          program->binary_sizes[device_i]);
