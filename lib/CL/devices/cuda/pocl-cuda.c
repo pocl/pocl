4902b123a3e9a583124f9d48f15be2f39a77f049
James Price <j.price@bristol.ac.uk>
2016-03-10 22:42:43 +0000

    [CUDA] Add device initialization

--- lib/CL/devices/cuda/pocl-cuda.c
+++ lib/CL/devices/cuda/pocl-cuda.c
@@ -27,17 +27,39 @@
 #include "common.h"
 #include "devices.h"
 
+#include <cuda.h>
+
+static void pocl_cuda_abort_on_error(CUresult result,
+                                     unsigned line,
+                                     const char* func,
+                                     const char* code,
+                                     const char* api)
+{
+  if (result != CUDA_SUCCESS)
+  {
+    const char *err_name;
+    const char *err_string;
+    cuGetErrorName(result, &err_name);
+    cuGetErrorString(result, &err_string);
+    POCL_MSG_PRINT2(func, line, "-> Error during %s\n", api);
+    POCL_ABORT("-> %s: %s\n", err_name, err_string);
+  }
+}
+
+#define CUDA_CHECK(result, api) \
+  pocl_cuda_abort_on_error(result, __LINE__, __FUNCTION__, #result, api);
+
+
 void
 pocl_cuda_init_device_ops(struct pocl_device_ops *ops)
 {
   pocl_basic_init_device_ops (ops);
 
-  /* TODO: more descriptive name from CUDA probing the device */
   ops->device_name = "CUDA";
   ops->init_device_infos = pocl_cuda_init_device_infos;
   ops->probe = pocl_cuda_probe;
-  //ops->uninit = pocl_cuda_uninit;
-  //ops->init = pocl_cuda_init;
+  ops->uninit = pocl_cuda_uninit;
+  ops->init = pocl_cuda_init;
   //ops->alloc_mem_obj = pocl_cuda_alloc_mem_obj;
   //ops->free = pocl_cuda_free;
   //ops->compile_submitted_kernels = pocl_cuda_compile_submitted_kernels;
@@ -49,6 +71,32 @@ pocl_cuda_init_device_ops(struct pocl_device_ops *ops)
   //ops->copy = pocl_cuda_copy;
   //ops->copy_rect = pocl_basic_copy_rect;
   //ops->get_timer_value = pocl_cuda_get_timer_value;
+}
+
+void
+pocl_cuda_init(cl_device_id device, const char* parameters)
+{
+  CUresult result;
+
+  result = cuInit(0);
+  CUDA_CHECK(result, "cuInit");
+
+  if (device->data)
+    return;
+
+  device->data = malloc(sizeof(CUdevice));
+  CUdevice *cuda_device = (CUdevice*)device->data;
+  result = cuDeviceGet(cuda_device, 0);
+  CUDA_CHECK(result, "cuDeviceGet");
+
+  // Get specific device name
+  device->long_name = device->short_name = malloc(256*sizeof(char));
+  cuDeviceGetName(device->long_name, 256, *cuda_device);
+
+  // Get other device properties
+  cuDeviceGetAttribute((int*)&device->max_compute_units,
+                       CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT,
+                       *cuda_device);
 }
 
 void
@@ -70,4 +118,13 @@ pocl_cuda_probe(struct pocl_device_ops *ops)
     return 1;
 
   return env_count;
+}
+
+void
+pocl_cuda_uninit(cl_device_id device)
+{
+  POCL_MEM_FREE(device->data);
+  device->data = NULL;
+
+  POCL_MEM_FREE(device->long_name);
 }
