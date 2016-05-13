26a1fda8cf065502561559a6019b741619175bbb
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2016-05-13 18:21:13 +0200

    Add test for user events
    There are a few issues with user events. The new test currently fails
    due to pocl assuming the status should be CL_COMPLETE on release,
    whereas a negative value would be acceptable too.

---
+++ tests/runtime/test_user_event.c
@@ -0,0 +1,68 @@
+/* Tests clCreateUserEvent(), clSetUserEventStatus() and operating on the user event
+
+   Copyright (c) 2016 Giuseppe Bilotta
+   
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+#include <CL/cl.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "poclu.h"
+
+#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(*arr))
+
+int main()
+{
+  cl_int err;
+  cl_event user_evt = NULL;
+  int i;
+
+  // An user event can be set to either complete or a negative value, indicating error;
+  // additionally, no objects involved in a command that waits on the user event should
+  // be released before the event status is set; however, it should be possible to release
+  // everything even if the status is set to something which is NOT CL_COMPLETE. So
+  // try both CL_COMPELTE and a negative value
+  cl_int status[] = {CL_INVALID_EVENT, CL_COMPLETE };
+
+  for (i = 0; i < ARRAY_SIZE(status); ++i) {
+	  cl_context context;
+	  cl_command_queue queue;
+	  cl_device_id device;
+
+	  CHECK_CL_ERROR(poclu_get_any_device(&context, &device, &queue));
+	  TEST_ASSERT( context );
+	  TEST_ASSERT( device );
+	  TEST_ASSERT( queue );
+
+	  user_evt = clCreateUserEvent(context, &err);
+	  CHECK_OPENCL_ERROR_IN("clCreateUserEvent");
+	  TEST_ASSERT( user_evt );
+
+	  CHECK_CL_ERROR(clSetUserEventStatus(user_evt, status[i]));
+	  CHECK_CL_ERROR(clReleaseEvent(user_evt));
+	  CHECK_CL_ERROR(clReleaseCommandQueue(queue));
+	  CHECK_CL_ERROR(clReleaseContext(context));
+  }
+
+  return EXIT_SUCCESS;
+
+}
