7817e61ba67291f1711fc43e7efd3fda4c545112
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2016-05-13 18:46:47 +0200

    Cleanup user event init
    * only set context and status if no error;
    * only set status once;
    * avoid potential null dereference when setting time_queue;
    * set refcount to 1.
    
    The last change in particular is important to alloww proper release of
    user events (otherwise a clReleaseEvent would fail on asserting that
    the decremented refcount was nonnegative). This is also consistent with
    command events which always start with a postive refcount.

--- lib/CL/clCreateUserEvent.c
+++ lib/CL/clCreateUserEvent.c
@@ -6,13 +6,10 @@ CL_API_ENTRY cl_event CL_API_CALL
 POname(clCreateUserEvent)(cl_context     context ,
                   cl_int *       errcode_ret ) CL_API_SUFFIX__VERSION_1_1
 {
-  int error; 
+  int error;
   cl_event event = NULL;
 
   error = pocl_create_event (&event, 0, CL_COMMAND_USER, 0, NULL, context);
-  
-  event->context = context;
-  event->status = CL_QUEUED;
 
   if (error != CL_SUCCESS)
     {
@@ -20,11 +17,12 @@ POname(clCreateUserEvent)(cl_context     context ,
     }
   else
     {
+      event->pocl_refcount = 1;
       event->status = CL_SUBMITTED;
+      event->context = context;
+      event->time_queue = pocl_gettimemono_ns();
     }
 
-  event->time_queue = pocl_gettimemono_ns();
-    
   if (errcode_ret)
     *errcode_ret = error;
 
