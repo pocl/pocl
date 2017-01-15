58e7baa5e6fd256f77f2ba2c124850bfe71be270
Arda Coskunses <acoskunses@gmail.com>
2017-01-14 20:47:03 -0700

    Pattern field SVM memfill command allocated as aligned. Matching free added as aligned free.

--- lib/CL/devices/common.c
+++ lib/CL/devices/common.c
@@ -538,6 +538,7 @@ pocl_exec_command (_cl_command_node * volatile node)
                                  node->command.memfill.pattern_size);
       POCL_UPDATE_EVENT_COMPLETE(event);
       POCL_DEBUG_EVENT_TIME(event, "SVM MemFill           ");
+      pocl_aligned_free(node->command.memfill.pattern);
       break;
     default:
       POCL_ABORT_UNIMPLEMENTED("");
