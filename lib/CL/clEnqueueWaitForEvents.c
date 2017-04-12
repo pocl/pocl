1f0f00b3c46d5f4e80d0f121735e727c752ddcfb
Michal Babej <michal.babej@tut.fi>
2017-04-12 16:31:35 +0200

    Fix a few problems in previous commits

--- lib/CL/clEnqueueWaitForEvents.c
+++ lib/CL/clEnqueueWaitForEvents.c
@@ -1,4 +1,6 @@
 #include "pocl_cl.h"
+#include "pocl_util.h"
+
 CL_API_ENTRY cl_int CL_API_CALL
 POname(clEnqueueWaitForEvents)(cl_command_queue  command_queue,
                        cl_uint           num_events,
