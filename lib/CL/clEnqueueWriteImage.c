ef21da26b09243e0ddd93f2c5f6fb4c72492bbbb
Michal Babej <michal.babej@tut.fi>
2017-04-13 13:20:05 +0200

    Fix NULL dereference in image checks

--- lib/CL/clEnqueueWriteImage.c
+++ lib/CL/clEnqueueWriteImage.c
@@ -26,9 +26,10 @@ POname(clEnqueueWriteImage)(cl_command_queue    command_queue,
   POCL_RETURN_ERROR_ON((command_queue->context != image->context),
     CL_INVALID_CONTEXT, "image and command_queue are not from the same context\n");
 
-  POCL_RETURN_ERROR_ON((image->buffer->flags & (CL_MEM_HOST_READ_ONLY | CL_MEM_HOST_NO_ACCESS)),
-    CL_INVALID_OPERATION, "image buffer has been created with CL_MEM_HOST_READ_ONLY "
-    "or CL_MEM_HOST_NO_ACCESS\n");
+  if (image->buffer)
+    POCL_RETURN_ERROR_ON((image->buffer->flags & (CL_MEM_HOST_READ_ONLY | CL_MEM_HOST_NO_ACCESS)),
+      CL_INVALID_OPERATION, "image buffer has been created with CL_MEM_HOST_READ_ONLY "
+      "or CL_MEM_HOST_NO_ACCESS\n");
 
   errcode = pocl_check_event_wait_list(command_queue, num_events_in_wait_list, event_wait_list);
   if (errcode != CL_SUCCESS)
