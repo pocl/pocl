a463628070130c3e37ddafe80993cbf048d8dd8c
Michal Babej <michal.babej@tut.fi>
2017-07-19 13:47:26 +0200

    pthread driver: do not save & restore ftz / rounding mode flags
    no need to do this for pthread, since it runs in its own threads.
    basic driver still needs it though.

--- lib/CL/devices/pthread/pthread_scheduler.c
+++ lib/CL/devices/pthread/pthread_scheduler.c
@@ -283,15 +283,10 @@ work_group_scheduler (kernel_run_command *k,
                  pc.group_id[1], pc.group_id[2]);
 #endif
 
-          unsigned rm = pocl_save_rm ();
           pocl_set_default_rm ();
-          unsigned ftz = pocl_save_ftz ();
           pocl_set_ftz (k->kernel->program->flush_denorms);
 
           k->workgroup (arguments, &pc);
-
-          pocl_restore_rm (rm);
-          pocl_restore_ftz (ftz);
 
         }
 
