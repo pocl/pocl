24caf3b10207cffed482a70cd79246959fdd299e
Michal Babej <michal.babej@tut.fi>
2017-11-27 17:27:56 +0100

    Add a test for issue #577

---
+++ tests/regression/test_issue_577.cpp
@@ -0,0 +1,42 @@
+
+// Trying to build a faulty program twice results in NULL deref
+// See https://github.com/pocl/pocl/issues/577
+// should print "BUILD ERROR" twice then "OK" once.
+
+#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_CL_1_2_DEFAULT_BUILD
+#include <CL/cl2.hpp>
+#include <iostream>
+
+const char *SOURCE = R"RAW(
+
+  __kernel void foo(__global int *input) {
+    !@#$%^&*();
+  }
+
+)RAW";
+
+int main(int argc, char *argv[]) {
+  cl_int err;
+  unsigned error_count = 0;
+  cl::Program program(SOURCE, false, &err);
+
+  for (unsigned i = 0; i < 2; i++) {
+    try {
+      program.compile();
+    } catch (cl::BuildError &e) {
+      std::cout << "BUILD ERROR\n";
+      error_count++;
+    }
+  }
+
+  if (error_count == 2) {
+    std::cout << "OK\n";
+    return 0;
+  } else {
+    std::cout << "FAIL\n";
+    return 1;
+  }
+}
