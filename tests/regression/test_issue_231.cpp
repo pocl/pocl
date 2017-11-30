78bba33b548efa0f25950dcb0f2a5b9b8596d3e5
Michal Babej <michal.babej@tut.fi>
2017-11-30 19:38:58 +0100

    Remove #define CL_HPP_CL_1_2_DEFAULT_BUILD from some tests
    This was apparently causing errors on Pekka's system:
    
    terminate called after throwing an instance of 'std::system_error'
      what():  Unknown error -1
    Aborted (core dumped)
    
    Reason unknown. The only thing the macro seems to do is
    not supply -cl-std=CL2.0 in options to clBuildProgram.

--- tests/regression/test_issue_231.cpp
+++ tests/regression/test_issue_231.cpp
@@ -5,7 +5,6 @@
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_TARGET_OPENCL_VERSION 120
-#define CL_HPP_CL_1_2_DEFAULT_BUILD
 #include <CL/cl2.hpp>
 #include <iostream>
 
