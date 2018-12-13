6cdef6e52255070f664417ed9617acd4ac2529e7
Pekka Jääskeläinen <pekka.jaaskelainen@parmance.com>
2018-12-13 18:12:12 +0200

    Add a matrix addition test case
    Also fix some strings in the vecadd CMakeLists.txt

---
+++ examples/matadd/matadd.c
@@ -0,0 +1,112 @@
+/* matadd - Matrix addition using work-items.
+
+   Copyright (c) 2018 Pekka Jääskeläinen
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/opencl.h>
+#include <poclu.h>
+
+#define N (16*16)
+#define M (8*16)
+
+#ifdef __cplusplus
+#  define CALLAPI "C"
+#else
+#  define CALLAPI
+#endif
+
+extern CALLAPI int
+exec_matadd_kernel (cl_context context, cl_device_id device,
+		    cl_command_queue cmd_queue, cl_program program,
+		    int n, int m, cl_float *srcA, cl_float *srcB,
+		    cl_float *dst);
+
+int
+main (int argc, char **argv)
+{
+  cl_float *srcA, *srcB;
+  cl_float *dst;
+  int i, j, err;
+
+  cl_context context = NULL;
+  cl_device_id device = NULL;
+  cl_platform_id platform = NULL;
+  cl_command_queue queue = NULL;
+  cl_program program = NULL;
+
+  err = poclu_get_any_device2 (&context, &device, &queue, &platform);
+  CHECK_OPENCL_ERROR_IN ("clCreateContext");
+
+  const char *basename = "matadd";
+  err = poclu_load_program (context, device, basename, 0, 0, 0,
+			    NULL, NULL, &program);
+  if (err != CL_SUCCESS)
+    goto FINISH;
+
+  srcA = (cl_float *) malloc (N*M * sizeof (cl_float));
+  srcB = (cl_float *) malloc (N*M * sizeof (cl_float));
+  dst = (cl_float *) malloc (N*M * sizeof (cl_float));
+
+  for (i = 0; i < N; ++i)
+    for (j = 0; j < M; ++j)
+      {
+	int indx = i*M + j;
+	srcA[indx] = (cl_float)indx;
+	srcB[indx] = (cl_float)(N*M - indx);
+	dst[indx] = (cl_float)-1;
+      }
+
+  err = 0;
+
+  if (exec_matadd_kernel (context, device, queue, program, N, M,
+			  srcA, srcB, dst))
+    {
+      printf ("Error running the tests\n");
+      err = 1;
+      goto FINISH;
+    }
+
+  for (i = 0; i < N; ++i)
+    for (j = 0; j < M; ++j)
+      {
+	int indx = i*M + j;
+	if ((int)srcA[indx] + (int)srcB[indx] != (int)dst[indx])
+	  {
+	    printf ("%d FAIL: %f + %f != %f\n", indx,
+		    srcA[indx], srcB[indx], dst[indx]);
+	    err = 1;
+	    goto FINISH;
+	  }
+      }
+
+  printf ("OK\n");
+
+FINISH:
+  CHECK_CL_ERROR (clReleaseProgram (program));
+  CHECK_CL_ERROR (clReleaseCommandQueue (queue));
+  CHECK_CL_ERROR (clUnloadPlatformCompiler (platform));
+  CHECK_CL_ERROR (clReleaseContext (context));
+
+  return err;
+}
---
+++ examples/matadd/matadd_exec.c
@@ -0,0 +1,100 @@
+/* matadd - Matrix addition using work-items.
+
+   Copyright (c) 2018 Pekka Jääskeläinen
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include "poclu.h"
+#include <CL/opencl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int
+exec_matadd_kernel (cl_context context, cl_device_id device,
+		    cl_command_queue cmd_queue, cl_program program,
+		    int n, int m, cl_float *srcA, cl_float *srcB,
+		    cl_float *dst)
+{
+  cl_kernel kernel = NULL;
+  cl_mem memobjs[3] = { 0, 0, 0 };
+  size_t global_work_size[] = {n, m};
+  cl_int err = CL_SUCCESS;
+  int i;
+
+  poclu_bswap_cl_float_array (device, (cl_float *)srcA, sizeof (cl_float) * n * m);
+  poclu_bswap_cl_float_array (device, (cl_float *)srcB, sizeof (cl_float) * n * m);
+
+  memobjs[0]
+    = clCreateBuffer (context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		      sizeof (cl_float) * n * m, srcA, &err);
+  CHECK_CL_ERROR2 (err);
+
+  memobjs[1]
+    = clCreateBuffer (context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		      sizeof (cl_float) * n * m, srcB, &err);
+  CHECK_CL_ERROR2 (err);
+
+  memobjs[2] = clCreateBuffer (context, CL_MEM_READ_WRITE,
+			       sizeof (cl_float) * n * m, NULL, &err);
+  CHECK_CL_ERROR2 (err);
+
+  kernel = clCreateKernel (program, "matadd", NULL);
+  CHECK_CL_ERROR2 (err);
+
+  err = clSetKernelArg (kernel, 0, sizeof (cl_mem), (void *)&memobjs[0]);
+  CHECK_CL_ERROR2 (err);
+
+  err = clSetKernelArg (kernel, 1, sizeof (cl_mem), (void *)&memobjs[1]);
+  CHECK_CL_ERROR2 (err);
+
+  err = clSetKernelArg (kernel, 2, sizeof (cl_mem), (void *)&memobjs[2]);
+  CHECK_CL_ERROR2 (err);
+
+  err = clEnqueueNDRangeKernel (cmd_queue, kernel, 2, NULL, global_work_size,
+				NULL, 0, NULL, NULL);
+  CHECK_CL_ERROR2 (err);
+
+  err = clEnqueueReadBuffer (cmd_queue, memobjs[2], CL_TRUE, 0,
+			     n * m * sizeof (cl_float), dst, 0, NULL, NULL);
+  CHECK_CL_ERROR2 (err);
+
+  poclu_bswap_cl_float_array (device, (cl_float *)dst,
+			      sizeof (cl_float) * n * m);
+  poclu_bswap_cl_float_array (device, (cl_float *)srcA,
+			      sizeof (cl_float) * n * m);
+  poclu_bswap_cl_float_array (device, (cl_float *)srcB,
+			      sizeof (cl_float) * n * m);
+
+ ERROR:
+  clReleaseMemObject (memobjs[0]);
+  clReleaseMemObject (memobjs[1]);
+  clReleaseMemObject (memobjs[2]);
+  clReleaseKernel (kernel);
+  return err;
+}
+
+#ifdef __cplusplus
+}
+#endif
