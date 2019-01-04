79f994a0eb6662401df64c41294216b964eed171
Michal Babej <michal.babej@tut.fi>
2019-01-04 16:38:48 +0200

    clEnqueueTask: handle internally like clEnqueueNDRangeKernel

--- lib/CL/clEnqueueTask.c
+++ lib/CL/clEnqueueTask.c
@@ -32,11 +32,10 @@ POname(clEnqueueTask)(cl_command_queue   command_queue,
               const cl_event *   event_wait_list,
               cl_event *         event ) CL_API_SUFFIX__VERSION_1_0
 {
-  cl_int status;
-  const size_t globalWS=1; 
-  const size_t localWS=1; 
+  const size_t globalWS=1;
+  const size_t localWS=1;
 
-  status = POname(clEnqueueNDRangeKernel) (command_queue,
+  return   POname(clEnqueueNDRangeKernel) (command_queue,
 					   kernel, 
 					   1 /*work_dim*/,
 					   NULL /*global_work_offset*/,
@@ -45,10 +44,5 @@ POname(clEnqueueTask)(cl_command_queue   command_queue,
 					   num_events_in_wait_list,
 					   event_wait_list,
 					   event);
-  if (event)
-    {
-      (*event)->command_type = CL_COMMAND_TASK;
-    }
-  return status;
 }
 POsym(clEnqueueTask)
