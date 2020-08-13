f32de3954e535529584be4a1de34f1c8b15fc00c
Michal Babej <michal.babej@tuni.fi>
2020-08-13 20:21:11 +0300

    Fix compatibility with LLVM <= 8

--- lib/llvmopencl/FlattenGlobals.cc
+++ lib/llvmopencl/FlattenGlobals.cc
@@ -114,7 +114,7 @@ bool FlattenGlobals::runOnModule(Module &M) {
         // if it's an OpenCL kernel with OptNone attribute, assume we're debugging,
         // and don't inline the kernel into the workgroup launcher.
         // this makes it possible to debug kernel code with GDB.
-        if (pocl::Workgroup::isKernelToProcess(*f) && f->hasOptNone())
+        if (pocl::Workgroup::isKernelToProcess(*f) && f->hasFnAttribute(Attribute::OptimizeNone))
           continue;
 
         functions_to_inline.insert(f);
