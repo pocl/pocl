eb4ae0e88ea2b806e624511f08d5922ba28e1e28
Matthias Diener <mdiener@illinois.edu>
2021-02-01 11:29:05 -0800

    fix DOMAIN_ID to the same value as used by the Nvidia CL driver

--- include/CL/cl_ext.h
+++ include/CL/cl_ext.h
@@ -240,7 +240,7 @@ typedef CL_API_ENTRY cl_command_queue
 #define CL_DEVICE_PCI_SLOT_ID_NV                    0x4009
 
 /* extension to cl_nv_device_attribute_query */
-#define CL_DEVICE_PCI_DOMAIN_ID_NV                  0x4010
+#define CL_DEVICE_PCI_DOMAIN_ID_NV                  0x400A
 
 
 /*********************************
