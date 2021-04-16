16b8a0963681b9229fa5373625c2a83c7f7909e1
Michal Babej <michal.babej@tuni.fi>
2021-04-16 10:03:36 +0300

    Temporarily revert LLVM 13 support for release 1.7 branch

--- cmake/LLVM.cmake
+++ cmake/LLVM.cmake
@@ -39,7 +39,6 @@ else()
     NAMES
       "llvmtce-config"
       "llvm-config"
-      "llvm-config-mp-13.0" "llvm-config-13" "llvm-config130"
       "llvm-config-mp-12.0" "llvm-config-12" "llvm-config120"
       "llvm-config-mp-11.0" "llvm-config-11" "llvm-config110"
       "llvm-config-mp-10.0" "llvm-config-10" "llvm-config100"
@@ -200,11 +199,8 @@ elseif(LLVM_VERSION MATCHES "^11[.]")
 elseif(LLVM_VERSION MATCHES "^12[.]")
   set(LLVM_MAJOR 12)
   set(LLVM_12_0 1)
-elseif(LLVM_VERSION MATCHES "^13[.]")
-  set(LLVM_MAJOR 13)
-  set(LLVM_13_0 1)
 else()
-  message(FATAL_ERROR "LLVM version between 6.0 and 13.0 required, found: ${LLVM_VERSION}")
+  message(FATAL_ERROR "LLVM version between 6.0 and 12.0 required, found: ${LLVM_VERSION}")
 endif()
 
 #############################################################
