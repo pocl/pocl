082ba567390af1007f53d411f6aff97b460ae770
Michal Babej <michal.babej@tuni.fi>
2021-05-18 11:49:01 +0300

    Split PyOpenCL tests

--- examples/PyOpenCL/runtest.sh
+++ examples/PyOpenCL/runtest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 TS_BASEDIR="$1"
-TS_BUILDDIR="$2"
-TS_SRCDIR="$3"
+shift
+TS_BUILDDIR="$1"
+shift
+TS_SRCDIR="$1"
+shift
 
-cd "${TS_SRCDIR}/test" && ${TS_BUILDDIR}/bin/py.test -v --tb=native
+cd "${TS_SRCDIR}/test" && ${TS_BUILDDIR}/bin/py.test -v --tb=native $@
