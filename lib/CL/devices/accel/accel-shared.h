8e2a2f837b8e60e74d1840eb5e0b863020467ff4
Michal Babej <michal.babej@tuni.fi>
2022-01-20 13:58:57 +0200

    Move builtin kernel related definitions to builtin_kernels.{hh,cc}

--- lib/CL/devices/accel/accel-shared.h
+++ lib/CL/devices/accel/accel-shared.h
@@ -10,6 +10,7 @@
 #include "MMAPDevice.h"
 #include "EmulationDevice.h"
 #include "bufalloc.h"
+#include "builtin_kernels.hh"
 
 #define ACCEL_DEFAULT_CTRL_SIZE (1024)
 
@@ -72,54 +73,6 @@
 #define AQL_MAX_SIGNAL_COUNT (5)
 
 #define DEFAULT_BUILD_HASH "ALMAIF_unset_hash"
-
-enum BuiltinKernelId : uint16_t
-{
-  // CD = custom device, BI = built-in
-  // 1D array byte copy, get_global_size(0) defines the size of data to copy
-  // kernel prototype: pocl.copy(char *input, char *output)
-  POCL_CDBI_COPY = 0,
-  POCL_CDBI_ADD32 = 1,
-  POCL_CDBI_MUL32 = 2,
-  POCL_CDBI_LEDBLINK = 3,
-  POCL_CDBI_COUNTRED = 4,
-  POCL_CDBI_JIT_COMPILER = 0xFFFF
-};
-
-// An initialization wrapper for kernel argument metadatas.
-struct BIArg : public pocl_argument_info
-{
-  BIArg (const char *TypeName, const char *Name, pocl_argument_type Type,
-         cl_kernel_arg_address_qualifier AQ)
-  {
-    type = Type;
-    address_qualifier = AQ;
-    type_name = strdup (TypeName);
-    name = strdup (Name);
-  }
-
-  ~BIArg ()
-  {
-    free (name);
-    free (type_name);
-  }
-};
-
-// An initialization wrapper for kernel metadatas.
-// BIKD = Built-in Kernel Descriptor
-struct BIKD : public pocl_kernel_metadata_t
-{
-  BIKD (BuiltinKernelId KernelId, const char *KernelName,
-        const std::vector<pocl_argument_info> &ArgInfos);
-
-  ~BIKD ()
-  {
-    delete[] arg_info;
-    free (name);
-  }
-
-  BuiltinKernelId KernelId;
-};
 
 struct AQLQueueInfo
 {
--- lib/CL/devices/accel/accel.cc
+++ lib/CL/devices/accel/accel.cc
@@ -71,44 +71,7 @@ void* runningThreadFunc(void*);
 _cl_command_node *runningList;
 bool runningJoinRequested = false;
 
-BIKD::BIKD(BuiltinKernelId KernelIdentifier, const char *KernelName,
-           const std::vector<pocl_argument_info> &ArgInfos)
-    : KernelId(KernelIdentifier) {
 
-  builtin_kernel = 1;
-  name = strdup(KernelName);
-  num_args = ArgInfos.size();
-  arg_info = new pocl_argument_info[num_args];
-  int i = 0;
-  for (auto ArgInfo : ArgInfos) {
-    arg_info[i] = ArgInfo;
-    arg_info[i].name = strdup(ArgInfo.name);
-    arg_info[i].type_name = strdup(ArgInfo.type_name);
-    ++i;
-  }
-}
-
-// Shortcut handles to make the descriptor list more compact.
-#define PTR_ARG POCL_ARG_TYPE_POINTER
-#define GLOBAL_AS CL_KERNEL_ARG_ADDRESS_GLOBAL
-
-BIKD BIDescriptors[] = {BIKD(POCL_CDBI_COPY, "pocl.copy",
-                             {BIArg("char*", "input", PTR_ARG, GLOBAL_AS),
-                              BIArg("char*", "output", PTR_ARG, GLOBAL_AS)}),
-                        BIKD(POCL_CDBI_ADD32, "pocl.add32",
-                             {BIArg("int*", "input", PTR_ARG, GLOBAL_AS),
-                              BIArg("int*", "input", PTR_ARG, GLOBAL_AS),
-                              BIArg("int*", "output", PTR_ARG, GLOBAL_AS)}),
-                        BIKD(POCL_CDBI_MUL32, "pocl.mul32",
-                             {BIArg("int*", "input", PTR_ARG, GLOBAL_AS),
-                              BIArg("int*", "input", PTR_ARG, GLOBAL_AS),
-                              BIArg("int*", "output", PTR_ARG, GLOBAL_AS)}),
-                        BIKD(POCL_CDBI_LEDBLINK, "pocl.ledblink",
-                             {BIArg("int*", "input", PTR_ARG, GLOBAL_AS),
-                              BIArg("int*", "input", PTR_ARG, GLOBAL_AS)}),
-                        BIKD(POCL_CDBI_COUNTRED, "pocl.countred",
-                             {BIArg("int*", "input", PTR_ARG, GLOBAL_AS),
-                              BIArg("int*", "output", PTR_ARG, GLOBAL_AS)})};
 
 
 
