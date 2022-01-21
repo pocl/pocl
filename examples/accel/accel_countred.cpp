8bd06f84ce753dc1c131a2a88385ecac9aae1227
Michal Babej <michal.babej@tuni.fi>
2022-01-21 18:51:35 +0200

    Fixes for examples/accel/accel_countred

--- examples/accel/accel_countred.cpp
+++ examples/accel/accel_countred.cpp
@@ -12,17 +12,17 @@ main(void)
     try {
         std::random_device rd;
         std::mt19937 mt(rd());
-        std::uniform_int_distribution<unsigned char> dist;
+        std::uniform_int_distribution<unsigned int> dist;
 
-        std::array<unsigned char, X * Y> framebuffer;
+        unsigned int framebuffer[X*Y];
         OpenCL_Manager mgr;
         mgr.initialize(X, Y);
         unsigned long redpixels = 0;
         while (true) {
-            for (size_t i = 0; i < X*Y ; ++i)
+            for (size_t i = 0; i < X*Y; ++i)
                 framebuffer[i] = dist(mt);
             redpixels = 0;
-            mgr.processCameraFrame(framebuffer.data(), &redpixels);
+            mgr.processCameraFrame(reinterpret_cast<unsigned char*>(framebuffer), &redpixels);
             std::cout << "FRAME red pixels: " << redpixels << "\n";
         }
     }
