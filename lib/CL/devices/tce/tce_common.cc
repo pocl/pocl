8183b402411e13cf0029bd2f262ef77c6d8e4c2e
Michal Babej <michal.babej@tuni.fi>
2022-01-27 12:22:50 +0200

    TCE: initial builtin kernel support

--- lib/CL/devices/tce/tce_common.cc
+++ lib/CL/devices/tce/tce_common.cc
@@ -35,6 +35,8 @@
 #include "pocl_runtime_config.h"
 #include "pocl_hash.h"
 #include "pocl_cache.h"
+#include "builtin_kernels.hh"
+#include "common_driver.h"
 
 #ifndef _MSC_VER
 #  include <unistd.h>
@@ -464,6 +466,9 @@ void pocl_tce_compile_kernel(_cl_command_node *Command, cl_kernel Kernel,
   if (!Device)
     Device = Command->device;
 
+  const char* Save;
+  sanitize_builtin_kernel_name(Kernel, &Save);
+
   POCL_LOCK(Dev->tce_compile_lock);
   int Error = pocl_llvm_generate_workgroup_function(
       Command->program_device_i, Device, Kernel, Command, Specialize);
@@ -473,7 +478,7 @@ void pocl_tce_compile_kernel(_cl_command_node *Command, cl_kernel Kernel,
     POCL_MSG_PRINT_GENERAL("TCE: pocl_llvm_generate_workgroup_function()"
                            " failed for kernel %s\n",
                            Kernel->name);
-    assert(Error == 0);
+    POCL_ABORT ("TCE compilation error\n");
   }
 
   // 12 == strlen (POCL_PARALLEL_BC_FILENAME)
@@ -515,8 +520,38 @@ void pocl_tce_compile_kernel(_cl_command_node *Command, cl_kernel Kernel,
     }
   }
 
+  restore_builtin_kernel_name(Kernel, Save);
+
   POCL_UNLOCK(Dev->tce_compile_lock);
 }
+
+int pocl_tce_build_builtin(cl_program program, cl_uint device_i)
+{
+  int err;
+
+  POCL_MSG_PRINT_TCE ("preparing OPENCL builtin kernels\n");
+  cl_device_id dev = program->devices[device_i];
+
+  assert (program->build_status == CL_BUILD_NONE);
+
+  uint64_t builtins_file_len = 0;
+  char* builtins_file = NULL;
+  if (pocl_read_file(SRCDIR "/lib/CL/devices/tce/builtins.cl",
+                     &builtins_file, &builtins_file_len) < 0)
+    {
+      POCL_MSG_ERR ("TCE: can't find opencl builtins");
+      return -1;
+    }
+
+  program->source = builtins_file;
+
+  err = pocl_driver_build_source (program, device_i, 0, NULL, NULL, 1);
+  POCL_RETURN_ERROR_ON( (err != CL_SUCCESS), CL_BUILD_PROGRAM_FAILURE,
+                        "failed to build OpenCL builtins for TCE\n");
+
+  return 0;
+}
+
 
 #define CHECK_AND_ALIGN_ARGBUFFER(DSIZE)                                      \
   do                                                                          \
