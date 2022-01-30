86bac172170b34aa217ed446f6bf18610c7f3007
Michal Babej <michal.babej@tuni.fi>
2022-01-30 18:23:36 +0200

    examples/accel/accel_example: update to use multiple builtin kernels
    If launched without arguments, tries to run an example using
    "pocl.add.i32" builtin kernel. To use a different builtin kernel,
    provide it as first argument. Supported builtin kernels:
    "pocl.add.i32", "pocl.mul.i32", "pocl.abs.f32", "pocl.sgemm.local.f32"

--- examples/accel/accel_example.cpp
+++ examples/accel/accel_example.cpp
@@ -1,160 +1,238 @@
-/* Tests the generic accelerator device driver.
 
-   Copyright (c) 2019 Pekka Jääskeläinen / Tampere University
 
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to deal
-   in the Software without restriction, including without limitation the rights
-   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
 
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-   THE SOFTWARE.
-*/
-
-// Enable OpenCL C++ exceptions
-#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_TARGET_OPENCL_VERSION 120
 #include <CL/opencl.hpp>
 
-#include <cstdio>
-#include <cstdlib>
+#include <string>
+#include <vector>
+#include <deque>
+#include <chrono>
+#include <mutex>
+#include <cmath>
+#include <condition_variable>
+#include <map>
+#include <set>
+#include <thread>
+#include <unordered_map>
+#include <random>
 #include <iostream>
 
-#ifndef _MSC_VER
-#  include <unistd.h>
-#else
-#  include "vccompat.hpp"
-#endif
+#define TILE 16
+#define X 640
+#define Y 640
+#define BUFSIZE (X*Y*4)
+
+#define CHECK_CL_ERROR(EXPR, ...) \
+    if (EXPR != CL_SUCCESS) { std::cerr << __VA_ARGS__; return 12; }
+
+
 
 int
-main(void)
+main(int argc, char** argv)
 {
-    try {
-        std::vector<cl::Platform> platformList;
+    cl::Platform platform;
+    std::vector<cl::Device> devices;
+    cl::Context ClContext;
 
-        // Pick platform
-        cl::Platform::get(&platformList);
+    cl::Device AccelDev;
+    cl::CommandQueue AccelQueue;
+    cl::Program AccelProgram;
+    cl::Kernel AccelKernel;
 
-        // Pick first platform
-        cl_context_properties cprops[] = {
-            CL_CONTEXT_PLATFORM, (cl_context_properties)(platformList[0])(), 0};
-        cl::Context context(CL_DEVICE_TYPE_CUSTOM, cprops);
+    cl::NDRange Offset, Global, Local;
+    int err;
 
-        // Query the set of devices attched to the context
-        std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
-
-        cl::Program program(context, devices, "pocl.add.i32;pocl.mul.i32");
-
-        // Build program
-        program.build(devices);
-
-        uint32_t in0[64];
-        uint32_t in1[64];
-        uint32_t in2[64];
-
-        uint32_t result[64];
-        for (size_t i = 0; i < 64; ++i) {
-            in0[i] = i + 777;
-            in1[i] = 2020 - i;
-            in2[i] = i + 9999;
-        }
-
-        cl::Buffer inBuffer0 = cl::Buffer(
-            context,
-            CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR,
-            (cl::size_type)64*sizeof(uint32_t),
-            (void*)in0);
-        cl::Buffer inBuffer1 = cl::Buffer(
-            context,
-            CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
-            (cl::size_type)64*sizeof(uint32_t),
-            (void*)in1);
-        cl::Buffer inBuffer2 = cl::Buffer(
-            context,
-            CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
-            (cl::size_type)64*sizeof(uint32_t),
-            (void*)in2);
-
-        cl::Buffer outBuffer0 = cl::Buffer(
-            context,
-            CL_MEM_READ_WRITE,
-            (cl::size_type)64*sizeof(uint32_t),
-            NULL);
-        cl::Buffer outBuffer1 = cl::Buffer(
-            context,
-            CL_MEM_READ_WRITE,
-            (cl::size_type)64*sizeof(uint32_t),
-            NULL);
-
-        // Create command queue
-        cl::CommandQueue queue(context, devices[0], 0);
-
-        // Create kernel object
-        cl::Kernel mul0(program, "pocl.mul.i32");
-        mul0.setArg(0, inBuffer0);
-        mul0.setArg(1, inBuffer1);
-        mul0.setArg(2, outBuffer0);
-
-        cl::Kernel mul1(program, "pocl.mul.i32");
-        mul1.setArg(0, inBuffer1);
-        mul1.setArg(1, inBuffer2);
-        mul1.setArg(2, outBuffer1);
-
-        cl::Kernel add(program, "pocl.add.i32");
-        add.setArg(0, outBuffer0);
-        add.setArg(1, outBuffer1);
-        add.setArg(2, inBuffer0);
-
-
-        //queue.enqueueWriteBuffer(inBuffer, CL_TRUE, 0, 128, &input[0], NULL, NULL);
-        queue.enqueueNDRangeKernel(mul0, cl::NullRange, cl::NDRange(64),
-                                   cl::NullRange);
-        queue.enqueueNDRangeKernel(mul1, cl::NullRange, cl::NDRange(64),
-                                   cl::NullRange);
-        queue.enqueueNDRangeKernel(add, cl::NullRange, cl::NDRange(64),
-                                   cl::NullRange);
-
-        uint32_t* Res = (uint32_t*) queue.enqueueMapBuffer(inBuffer0, CL_FALSE,
-                                                           CL_MAP_READ, 0, 256);
-
-        queue.finish();
-
-        int correct = 1;
-        for (size_t i = 0; i < 64; ++i) {
-            uint32_t value = Res[i];
-            uint32_t expected = (i + 777)*(2020-i) + (2020-i)*(i+9999);
-            if (value != expected) {
-                printf("at idx %zd expected %i, got %i\n", i, expected, value);
-                correct = 0;
-            }
-
-        }
-        if (correct == 1) {
-            printf("OK: Correct result!\n");
-        }
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+    if(!all_platforms.size()) {
+        std::cerr << "No OpenCL platforms available!\n";
+        return 1;
     }
-    catch (cl::Error &err) {
-         std::cerr
-             << "ERROR: "
-             << err.what()
-             << "("
-             << err.err()
-             << ")"
-             << std::endl;
 
-         return EXIT_FAILURE;
+    platform = all_platforms[0];
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+    if(devices.size() == 0) {
+        std::cerr << "No OpenCL devices available!\n";
+        return 2;
     }
+
+    const char* kernel_name = "pocl.add.i32";
+    const char* known_kernels[] = { "pocl.add.i32", "pocl.mul.i32",
+                                    "pocl.abs.f32",
+                                    "pocl.sgemm.local.f32",
+                                    NULL,
+                                  };
+    if (argc > 1)
+      {
+        kernel_name = argv[1];
+        std::string k{kernel_name};
+        int found = 0;
+        unsigned i = 0;
+        while (known_kernels[i]) {
+            if (k.compare(known_kernels[i]) == 0) found = 1;
+            ++i;
+          }
+        if (!found) {
+            std::cerr << "unknown builtin kernel: " << kernel_name << "\n";
+            return 3;
+          }
+      }
+
+    std::string kernel_str{kernel_name};
+
+    for (auto& D : devices) {
+      std::string AccelBuiltinKernels = D.getInfo<CL_DEVICE_BUILT_IN_KERNELS>();
+      std::cout << "Device " << D.getInfo<CL_DEVICE_NAME>() <<
+                   " has builtin kernels: " << AccelBuiltinKernels << "\n";
+      if (AccelBuiltinKernels.find(kernel_str) != std::string::npos)
+      {
+         AccelDev = D;
+         break;
+      }
+    }
+    if (AccelDev.get() == nullptr) {
+        std::cerr << "no devices which support builtin kernel " << kernel_str << "\n";
+        return 4;
+      }
+
+    std::cout << "Using device: " << AccelDev.getInfo<CL_DEVICE_NAME>() << "\n";
+    std::cout << "Using builtin kernel: " << kernel_str << "\n";
+
+    std::vector<cl::Device> AccelDevs = {AccelDev};
+    ClContext = cl::Context(AccelDevs, nullptr, nullptr, nullptr, &err);
+    CHECK_CL_ERROR(err, "Context creation failed\n");
+
+    AccelQueue = cl::CommandQueue(ClContext, AccelDev, 0, &err); // , CL_QUEUE_PROFILING_ENABLE
+    CHECK_CL_ERROR(err, "CmdQueue creation failed\n");
+
+    AccelProgram = cl::Program{ClContext, AccelDevs, kernel_str.c_str(), &err};
+    CHECK_CL_ERROR(err, "Program creation failed\n");
+
+    err = AccelProgram.build(AccelDevs);
+    CHECK_CL_ERROR(err, "Program build failed\n");
+    cl::Kernel Kernel = cl::Kernel(AccelProgram, kernel_str.c_str(), &err);
+    CHECK_CL_ERROR(err, "Kernel creation failed\n");
+
+    cl::Buffer Input1 = cl::Buffer(ClContext, CL_MEM_READ_WRITE, (cl::size_type)BUFSIZE, nullptr, &err);
+    CHECK_CL_ERROR(err, "Input1 buffer creation failed\n");
+    cl::Buffer Input2 = cl::Buffer(ClContext, CL_MEM_READ_WRITE, (cl::size_type)BUFSIZE, nullptr, &err);
+    CHECK_CL_ERROR(err, "Input2 buffer creation failed\n");
+    cl::Buffer Out1 = cl::Buffer(ClContext, CL_MEM_READ_WRITE, (cl::size_type)BUFSIZE, nullptr, &err);
+    CHECK_CL_ERROR(err, "OUtput1 buffer creation failed\n");
+
+    void *i1, *i2, *o1;
+    Offset = cl::NullRange;
+    Local = cl::NDRange(TILE, TILE);
+    Global = cl::NDRange(X, Y);
+
+    if (kernel_str.compare("pocl.add.i32") == 0 ||
+        kernel_str.compare("pocl.mul.i32") == 0)
+    {
+        Kernel.setArg(0, Input1);
+        Kernel.setArg(1, Input2);
+        Kernel.setArg(2, Out1);
+    }
+
+    if (kernel_str.compare("pocl.abs.f32") == 0)
+    {
+        Kernel.setArg(0, Input1);
+        Kernel.setArg(1, Out1);
+    }
+
+    if (kernel_str.compare("pocl.sgemm.local.f32") == 0)
+    {
+        Kernel.setArg(0, Input1);
+        Kernel.setArg(1, Input2);
+        Kernel.setArg(2, Out1);
+        Kernel.setArg(3, X);
+        Kernel.setArg(4, Y);
+        Kernel.setArg(5, X);
+    }
+
+    if (kernel_str.compare("pocl.sgemm.local.f32") == 0 ||
+        kernel_str.compare("pocl.abs.f32") == 0)
+    {
+        std::mt19937 mt{234545649UL};
+        std::uniform_real_distribution<float> dist(15.0, 25.0);
+
+        float* in1 = new float[X*Y];
+        float* in2 = new float[X*Y];
+        float* out1 = new float[X*Y];
+
+        for (size_t i = 0; i < X*Y; ++i) {
+            in1[i] = dist(mt);
+            in2[i] = dist(mt);
+            out1[i] = 0.0f;
+          }
+        i1 = in1; i2 = in2; o1 = out1;
+    }
+    else
+    {
+        std::mt19937 mt{234545649UL};
+        std::uniform_int_distribution<unsigned int> dist(10, 24);
+
+        uint32_t *in1 = new uint32_t[X*Y];
+        uint32_t *in2 = new uint32_t[X*Y];
+        uint32_t *out1 = new uint32_t[X*Y];
+
+        for (size_t i = 0; i < X*Y; ++i) {
+            in1[i] = dist(mt);
+            in2[i] = dist(mt);
+            out1[i] = 0;
+          }
+        i1 = in1; i2 = in2; o1 = out1;
+      }
+
+    using clock_type = std::chrono::steady_clock;
+    using second_type = std::chrono::duration<double, std::ratio<1> >;
+    std::chrono::time_point<clock_type> m_beg { clock_type::now() };
+
+    err = AccelQueue.enqueueWriteBuffer(Input1, CL_FALSE, 0, BUFSIZE, i1);
+    CHECK_CL_ERROR(err, "en 1");
+    err = AccelQueue.enqueueWriteBuffer(Input2, CL_FALSE, 0, BUFSIZE, i2);
+    CHECK_CL_ERROR(err, "en 2");
+
+    err = AccelQueue.enqueueNDRangeKernel(Kernel, Offset, Global, Local);
+    CHECK_CL_ERROR(err, "en 3");
+    err = AccelQueue.enqueueReadBuffer(Out1, CL_TRUE, 0, BUFSIZE, o1);
+    CHECK_CL_ERROR(err, "en 4");
+
+    std::chrono::time_point<clock_type> m_end { clock_type::now() };
+    double diff = std::chrono::duration_cast<second_type>(m_end - m_beg).count();
+    std::cout << "Execution time(s): " << diff << "\n\n";
+
+    if (kernel_str.compare("pocl.sgemm.local.f32") == 0 ||
+        kernel_str.compare("pocl.abs.f32") == 0)
+    {
+        float* in1 = (float *)i1;
+        float* in2 = (float *)i2;
+        float* out1 = (float *)o1;
+        for (size_t i = 0; i < 10; ++i)
+        {
+           std::cout << "IN1: " << in1[i] << "  IN2: " << in2[i] <<
+                     "  OUT1: " << out1[i] << "\n";
+        }
+        delete [] in1;
+        delete [] in2;
+        delete [] out1;
+    }
+    else
+    {
+        uint32_t *in1 = (uint32_t *)i1;
+        uint32_t *in2 = (uint32_t *)i2;
+        uint32_t *out1 = (uint32_t *)o1;
+        for (size_t i = 0; i < 10; ++i)
+        {
+           std::cout << "IN1: " << in1[i] << "  IN2: " << in2[i] <<
+                     "  OUT1: " << out1[i] << "\n";
+        }
+        delete [] in1;
+        delete [] in2;
+        delete [] out1;
+      }
 
     return EXIT_SUCCESS;
 }
