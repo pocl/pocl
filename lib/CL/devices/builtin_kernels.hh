c44832b45bc5e95d9b3b0a4eba72f36269669da7
Michal Babej <michal.babej@tuni.fi>
2022-02-08 16:59:38 +0200

    Builtin_kernel: add documentation on adding kernels

--- lib/CL/devices/builtin_kernels.hh
+++ lib/CL/devices/builtin_kernels.hh
@@ -1,6 +1,35 @@
 #include "pocl_cl.h"
 #include "pocl_export.h"
 
+/*
+  steps to add a new builtin kernel:
+
+  1) add it to the end of BuiltinKernelId enum in this file
+
+  2) open builtin_kernels.cc and edit BIDescriptors, add a new struct
+     for the new kernel, with argument metadata
+
+  3) make sure that devices where you want to support this builtin kernel,
+     report it. Every driver does this a bit differently, but at pocl_XYZ_init
+     it must properly fill dev->builtin_kernel_list, dev->num_builtin_kernels
+     Note: the kernel name reported to user should use dots as separators
+     (example: pocl.add.apples.to.oranges)
+
+  4) add the code for the builtin kernel for each device that will support it.
+     Note: if the builtin kernel is in source format, its name in the source
+     MUST have the dots replaced with underscore
+     (example: pocl_add_apples_to_oranges)
+
+     How to do this, depends on device:
+       * CUDA has OpenCL-source builtins in lib/CL/devices/cuda/builtins.cl,
+         it also has CUDA-source builtins in lib/CL/devices/cuda/builtins.cu
+       * accel driver with TTASIM backend has opencl-source builtins in
+         lib/CL/devices/accel/tce_builtins.cl
+       * accel driver with other backends has builtin kernels in binary format (bitstream)
+
+
+*/
+
 #ifndef POCL_BUILTIN_KERNELS_H
 #define POCL_BUILTIN_KERNELS_H
 
