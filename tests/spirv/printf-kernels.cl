de9b966bcfc2e335cfcca89e9e82271d517426bc
Pekka Jääskeläinen <pekka.jaaskelainen@parmance.com>
2022-02-09 18:41:20 +0200

    Test cases for SPIRV/printf

---
+++ tests/spirv/printf-kernels.cl
@@ -0,0 +1,125 @@
+/* printf-kernels.cl - Test cases for SPIR-V printf (the kernels).
+
+   Copyright (c) 2022 Pekka Jääskeläinen / Parmance
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+*/
+
+// Example build steps:
+//
+// clang -cc1 -triple=spir64-unknown-unknown -cl-std=CL2.0 -include \
+// ~/src/llvm/clang/lib/Headers/opencl-c.h printf-kernels.cl \
+// -emit-llvm-bc -o printf-kernels.bc
+//
+// llvm-spirv printf-kernels.bc
+//
+// spirv-val printf-kernels.spv
+
+global char hello_g[] = "HELLO GLOBAL\n";
+global char strings_g[] = "STRiNGS\n";
+
+/// Cases that should adhere to both the SPIR-V and OpenCL 1.2 printf ///
+
+kernel void ocl_std_no_args(global int *io) {
+  io[0] = printf("Hello\n");
+}
+
+kernel void ocl_std_literal_str_arg(global int *io) {
+  io[0] = printf("%s", "Hello");
+  io[0] += printf(" %s\n", "strings.");
+}
+
+kernel void ocl_std_nop_str_arg(global int *io) {
+  printf("Howdy HIPpies!\n");
+  io[0] = printf("%s", "");
+  io[0] += printf("");
+  io[0] += printf("I'm correct since I'm not an empty string!\n");
+}
+
+// OpenCL 1.2-compliant fallback to print strings that are not in the
+// constant memory.
+void putstr(global char *str) {
+  global char *pos = str;
+  while (*pos) {
+    printf("%c", *pos);
+    ++pos;
+  }
+}
+
+kernel void ocl_std_global_str_arg_emulated(global int *io) {
+  putstr(hello_g);
+  putstr(strings_g);
+  io[0] = 0;
+}
+
+/// Useful cases that adhere to the SPIR-V/OpenCL/printf specs wording,
+/// but not strictly to the OpenCL 1.2 printf specs, which the implementations
+/// thus _might_ support (like PoCL in some cases does) ///
+
+// This does not adhere to the OpenCL specs as the wording is "string literals"
+// when defining %s args, thus the arg itself should be a literal, and cannot
+// be given via a variable (even though the variable has a literal assigned to
+// it directly and is in constant AS)? Clang 14 seems to digest this case
+// without noise.
+constant const char *hello_c_p = "CONSTANT AS program scope\n";
+kernel void spirv_std_var_str_arg_constant_program_scope(global int *io) {
+  io[0] = printf("%s", hello_c_p);
+}
+
+// This case is specifically given as an example of a non-valid use
+// in the OpenCL 1.2 specs.
+kernel void spirv_std_var_str_arg_constant_func_scope(global int *io) {
+  constant const char *hello_c_f = "CONSTANT AS function scope\n";
+  io[0] = printf("%s", hello_c_f);
+}
+
+// This is not allowed by OpenCL 1.2 standard which requires %s to originate
+// from string literals and the string literals are supposed to be in constant
+// AS.
+kernel void spriv_std_str_arg_global(global int *io) {
+  io[0] = printf("%s", hello_g);
+  io[0] += printf(" %s\n", strings_g);
+}
+
+// Dynamic selection of the constant string.
+kernel void spirv_std_conditional_string(global int *io) {
+  constant const char *dyn_a = "io[0] was 1";
+  constant const char *dyn_b = "io[0] was something else than 1, it was";
+  constant const char *str = NULL;
+
+  if (io[0] == 1) {
+    str = dyn_a;
+  } else {
+    str = dyn_b;
+  }
+  io[0] = printf("%s %d\n", str, io[0]);
+}
+
+kernel void spirv_std_array_of_strings(global int* io) {
+  constant const char *dyn_a = "I am a dynamic str arg A.\n";
+  constant const char *dyn_b = "I am a dynamic str arg B.\n";
+
+  constant const char *array_of_strings[] = {dyn_a, dyn_b};
+
+  io[0] = printf("%s\n", array_of_strings[io[0]]);
+}
+
+kernel void spirv_host_defined_strings(global int* io, global char *str) {
+  io[0] = printf("host defined string: %s", str);
+}
---
+++ tests/spirv/printf.cc
@@ -0,0 +1,156 @@
+/* printf-opencl.cc - Test cases for SPIR-V printf (the host program).
+
+   Copyright (c) 2022 Pekka Jääskeläinen / Parmance
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+*/
+
+#include <CL/cl.h>
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string.h>
+
+#define CHECK_ERR(X) do {					\
+    if ((X) != CL_SUCCESS) {					\
+      std::cerr << #X "ERROR at " << __LINE__ << std::endl;	\
+      abort();							\
+    } } while (0)
+
+std::vector<std::string> getKernelNames(cl_program Program) {
+  size_t KernelNamesSize;
+  CHECK_ERR(clGetProgramInfo(Program, CL_PROGRAM_KERNEL_NAMES,
+			     0, NULL,
+			     &KernelNamesSize));
+
+
+  char Names[KernelNamesSize + 1];
+  CHECK_ERR(clGetProgramInfo(Program, CL_PROGRAM_KERNEL_NAMES,
+			     KernelNamesSize, &Names[0],
+			     &KernelNamesSize));
+
+  // The specs doesn't ensure a null terminated string.
+  Names[KernelNamesSize] = 0;
+
+  std::vector<std::string> KernelNames;
+  //Construct a stream from the string
+  std::stringstream StreamData(Names);
+  std::string Kern;
+  while (std::getline(StreamData, Kern, ';')) {
+    KernelNames.push_back(Kern);
+  }
+  return KernelNames;
+}
+
+int main(int Argc, char *Argv[]) {
+
+  cl_uint Count;
+  cl_platform_id Platform;
+
+  clGetPlatformIDs (1, &Platform, &Count);
+  if (Count == 0)
+    abort();
+
+  cl_context_properties Properties[] =
+    {CL_CONTEXT_PLATFORM, (cl_context_properties)Platform, 0};
+
+  cl_context Context =
+    clCreateContextFromType (Properties, CL_DEVICE_TYPE_ALL,
+			     NULL, NULL, NULL);
+
+  cl_uint NumDevices;
+  cl_int Err = clGetDeviceIDs(Platform, CL_DEVICE_TYPE_ALL, 0, NULL,
+			      &NumDevices);
+  if (Err != CL_SUCCESS || NumDevices == 0)
+    return EXIT_FAILURE;
+
+  cl_device_id Device;
+  Err = clGetDeviceIDs (Platform, CL_DEVICE_TYPE_ALL, 1, &Device, NULL);
+  if (Err != CL_SUCCESS)
+    return EXIT_FAILURE;
+
+  std::ifstream File(SRCDIR "/printf-kernels.spv", std::ios::binary | std::ios::ate);
+  std::streamsize Binsize = File.tellg();
+  File.seekg(0, std::ios::beg);
+
+  std::vector<char> Binary(Binsize);
+  if (!File.read(Binary.data(), Binsize))
+    return EXIT_FAILURE;
+
+  cl_program Program =
+    clCreateProgramWithIL (Context, (const void *)Binary.data(), Binsize,
+			   &Err);
+  CHECK_ERR(Err);
+
+  Err = clBuildProgram (Program, 0, NULL, "", NULL, NULL);
+  CHECK_ERR(Err);
+
+  cl_command_queue Queue =
+    clCreateCommandQueueWithProperties(Context, Device, 0, &Err);
+  CHECK_ERR(Err);
+
+  auto KernelNames = getKernelNames(Program);
+
+  for (auto KernelName : KernelNames) {
+    std::cout << "Testing '" << KernelName << "'" << std::endl;
+    cl_kernel Kernel = clCreateKernel (Program, KernelName.c_str(), NULL);
+
+    int IO = 1;
+    char HostStr[] = "String from the HOST\n\0";
+
+    cl_mem Buffer =
+      clCreateBuffer (Context,
+		      CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+		      sizeof(int), &IO, &Err);
+    CHECK_ERR(Err);
+
+    Err = clSetKernelArg (Kernel, 0, sizeof (cl_mem), (void *)&Buffer);
+    CHECK_ERR(Err);
+
+    cl_mem Buffer2;
+    if (KernelName == "spirv_host_defined_strings") {
+      Buffer2 =
+	clCreateBuffer (Context,
+			CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+			strlen(HostStr) + 1, &HostStr, &Err);
+      Err = clSetKernelArg (Kernel, 1, sizeof (cl_mem), (void *)&Buffer2);
+      CHECK_ERR(Err);
+    }
+
+    const size_t LocalSize[] = {1, 1, 1};
+    const size_t GlobalSize[] = {1, 1, 1};
+
+    Err = clEnqueueNDRangeKernel (Queue, Kernel, 1, NULL, LocalSize,
+				  GlobalSize, 0, NULL, NULL);
+    CHECK_ERR(Err);
+
+    Err = clEnqueueReadBuffer (Queue, Buffer, CL_TRUE, 0,
+			       sizeof(int), &IO, 0, NULL, NULL);
+    CHECK_ERR(Err);
+
+    clFinish(Queue);
+
+    clReleaseMemObject(Buffer);
+    std::cout << "IO == " << IO << std::endl << std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
