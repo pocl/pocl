2a32472666de0cb16aaa38ede308f0cebc8d6314
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2023-01-27 15:22:14 +0100

    Run tests with `loopvec` not `loops` WIH.

--- cmake/add_test_pocl.cmake
+++ cmake/add_test_pocl.cmake
@@ -51,7 +51,7 @@ function(add_test_pocl)
   if(POCL_TEST_WORKITEM_HANDLER)
     set(VARIANTS ${POCL_TEST_WORKITEM_HANDLER})
   else()
-    set(VARIANTS "loops" "cbs")
+    set(VARIANTS "loopvec" "cbs")
   endif()
   list(LENGTH VARIANTS VARIANTS_COUNT)
 
