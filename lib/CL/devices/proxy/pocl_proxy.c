3d89229b6c0580d7774838dc3d6d15cb7fbef176
Jan Solanti <jan.solanti@tuni.fi>
2023-05-19 18:47:03 +0300

    proxy: fix device->available initialization

--- lib/CL/devices/proxy/pocl_proxy.c
+++ lib/CL/devices/proxy/pocl_proxy.c
@@ -510,7 +510,7 @@ pocl_proxy_get_device_info (cl_device_id device, proxy_device_data_t *d)
 
   // TODO queue properties
   device->queue_properties = CL_QUEUE_PROFILING_ENABLE;
-  DIboolptr (available, CL_DEVICE_AVAILABLE);
+  DIbool (available[0], CL_DEVICE_AVAILABLE);
   DIbool (compiler_available, CL_DEVICE_COMPILER_AVAILABLE);
   DIbool (linker_available, CL_DEVICE_LINKER_AVAILABLE);
 
