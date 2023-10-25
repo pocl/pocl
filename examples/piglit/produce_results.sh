e29381186f162462bcba51dbf5d20c48303e7f0b
Michal Babej <michal.babej@intel.com>
2023-10-25 12:15:50 +0300

    examples/piglit: remove old unused files

--- examples/piglit/produce_results.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# The below test cases seem to pass indeterministically. Filter them out
-# _for now_ to make regression checking feasible.
-piglit/piglit-run.py -v piglit/tests/cl.tests piglit/results/all 2>&1 | egrep "^pass:" | \
-egrep -v "program@execute@builtin@builtin-float-asinh-1.0.generated|\
-program@execute@store@store-uint2-global" > result
-sed -i "s/[ \t]*$//" result
-LC_ALL=C sort result -o sorted_result
