74f0357041666b9fcd3a39e9b4d27ae287b62eb0
Pekka Jääskeläinen <pekka.jaaskelainen@intel.com>
2024-03-14 11:33:57 +0200

    format-diff.sh: allow passing parameters to git diff
    Passing '--staged' is useful for formatting newly added files.

--- tools/scripts/format-diff.sh
+++ tools/scripts/format-diff.sh
@@ -15,7 +15,7 @@ pushd ${GITROOT} > /dev/null
 PATCHY=$(mktemp /tmp/pocl.XXXXXXXX.patch)
 trap "rm -f $PATCHY" EXIT
 
-git diff -U0 --no-color >$PATCHY
+git diff $* -U0 --no-color >$PATCHY
 
 $RELPATH/clang-format-diff.py -regex '.*(\.h$|\.c$|\.cl$)' -i -p1 -style GNU <$PATCHY
 $RELPATH/clang-format-diff.py -regex '(.*(\.hpp$|\.hh$|\.cc$|\.cpp$))|(lib/llvmopencl/.*)|(lib/CL/devices/tce/.*)' -i -p1 -style LLVM <$PATCHY
