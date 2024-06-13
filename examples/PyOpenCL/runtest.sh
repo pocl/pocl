27895d9163e8f5b07edf711ae2d4396a2faf8104
Pekka Jääskeläinen <pekka.jaaskelainen@intel.com>
2024-06-13 17:01:16 +0300

    Debugging aids to the PyOpenCL test
    DBG_CMD can be used to run it under gdb
    -s allows dumping POCL_DEBUGs right away

--- examples/PyOpenCL/runtest.sh
+++ examples/PyOpenCL/runtest.sh
@@ -7,4 +7,8 @@ shift
 TS_SRCDIR="$1"
 shift
 
-cd "${TS_SRCDIR}/test" && ${TS_BUILDDIR}/bin/py.test -v --tb=native $@
+# Set DBG_CMD="gdb --args" to drop to a debugger.
+# -s is useful as it dumps the PoCL logs directly without capture.
+# -k can be used to filter the test
+
+cd "${TS_SRCDIR}/test" && ${DBG_CMD} ${TS_BUILDDIR}/bin/python3 ${TS_BUILDDIR}/bin/py.test -s -v --tb=native $@
