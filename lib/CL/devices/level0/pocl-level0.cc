5e56f9dc96f80a0b42429902b9ef0341b921dae1
Michal Babej <michal.babej@intel.com>
2024-07-30 19:40:09 +0300

    Level Zero: fix bug in pocl_level0_link_program() found by CTS compiler test

--- lib/CL/devices/level0/pocl-level0.cc
+++ lib/CL/devices/level0/pocl-level0.cc
@@ -666,7 +666,15 @@ int pocl_level0_link_program(cl_program Program, cl_uint DeviceI,
     assert(Dev == InputPrograms[I]->devices[DeviceI]);
     POCL_LOCK_OBJ(InputPrograms[I]);
 
+    pocl_cache_program_spv_path(ProgramSpvPath, InputPrograms[I], DeviceI);
+    assert(pocl_exists(ProgramSpvPath));
+    SpvBinaryPaths.push_back(ProgramSpvPath);
+
     char *Spv = (char *)InputPrograms[I]->program_il;
+    if (Spv == nullptr) {
+      readProgramSpv(InputPrograms[I], DeviceI, ProgramSpvPath);
+    }
+    Spv = (char *)InputPrograms[I]->program_il;
     assert(Spv);
     size_t Size = InputPrograms[I]->program_il_size;
     assert(Size);
@@ -677,10 +685,6 @@ int pocl_level0_link_program(cl_program Program, cl_uint DeviceI,
       assert(pocl_exists(ProgramBcPath));
       BcBinaryPaths.push_back(ProgramBcPath);
     }
-
-    pocl_cache_program_spv_path(ProgramSpvPath, InputPrograms[I], DeviceI);
-    assert(pocl_exists(ProgramSpvPath));
-    SpvBinaryPaths.push_back(ProgramSpvPath);
 
     POCL_UNLOCK_OBJ(InputPrograms[I]);
   }
