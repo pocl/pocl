345b2b16de42a773e304db696e63d6eaafdaec73
Michal Babej <90404+franz@users.noreply.github.com>
2024-11-08 09:40:02 +0200

    remove a comment about an old problem that has been fixed

--- lib/CL/clReleaseContext.c
+++ lib/CL/clReleaseContext.c
@@ -122,27 +122,12 @@ POname(clReleaseContext)(cl_context context) CL_API_SUFFIX__VERSION_1_0
 }
 POsym(clReleaseContext)
 
-/*
-  This is a workaround for a problem explained below.
-
-  Note this is function *only* used by pocl tests, is subject to change without notice,
-  and only available when compiling in no-ICD mode (= test is directly linked to libpocl).
-*/
-
 void
 pocl_check_uninit_devices ()
 {
-  /* This is a sleep with rather arbitrary amount, but it's required.
-   * See the full explanation below, but basically,
-   * this gives the pocl driver a bit of time to cleanup
-   * and go idle, so we can safely call pocl_uninit_devices() from
-   * a user thread. */
-
   int do_uninit = pocl_get_bool_option ("POCL_ENABLE_UNINIT", 0);
   if (!do_uninit)
     return;
-
-  usleep (100000);
 
   POCL_LOCK (pocl_context_handling_lock);
   if (cl_context_count == 0)
@@ -185,74 +170,3 @@ pocl_check_uninit_devices ()
 
   POCL_UNLOCK (pocl_context_handling_lock);
 }
-
-
-/*
-
-There used to be code that called pocl_uninit_devices() from clReleaseContext()
-to un-initialize all devices when cl_context_count reached 0.
-Unfortunately it ignores some realities of OpenCL.
-
-What happens (usually) is:
-
-  Application     Pocl driver thread(s)
------------------------------------------------------------------------
-  clEnqueueSomething()
-                            process command
-
-  clFinish()
-   .. in libpocl: wait for notify from driver
-
-                            process any commands
-                            notify user app thread (via pthread_cond_signal) that command queue is empty
-                            release any cl_objects used by last command (decrease refcounts)
-
-  clReleaseBuffer() etc
-  clReleaseCommandQueue()
-  clReleaseContext()
-   .. in libpocl: decrease context's refcount
-     .. context refcount is 0 -> call pocl_uninit_devices()
-
-                            uninit_device() called, unitialize device, stop driver threads etc
-
-What can happen instead is:
-
-  Application               Pocl driver thread(s)
------------------------------------------------------------------------
-  clEnqueueSomething()
-                            process command
-
-  clFinish()
-   .. in libpocl: wait for notify from driver
-
-                            process any commands
-                            notify user app thread (via pthread_cond_signal) that command queue is empty
-                            driver thread gets unscheduled by OS
-
-  clReleaseBuffer() etc
-  clReleaseCommandQueue()
-  clReleaseContext()
-   .. in libpocl: decrease context's refcount
-     .. context refcount is NOT 0 (because driver hasn't clReleased cl_objects used by last command yet)
-
-** POINT X ** (see below)
-
-                            release any cl_objects used by commands:
-                            clReleaseEvent()
-                            clReleaseCommandQueue()
-                            clReleaseContext(): now refcount is 0
-                                -> calls pocl_uninit_devices() ** from the driver thread **
-The symptoms are
-  1) pocl_uninit_devices() is called from a driver thread - it would have to release itself
-  2) at "POINT X" the application usually calls exit() which also causes the driver threads to terminate,
-     leaving unreleased objects, triggering sanitizer checks
-
-The root of the problem is that the driver notifies the user app when the command finished executing,
-but before it has done the clRelease* calls on commands/buffers etc of the command's event. This *may*
-be possible to fix but possibly requires significant changes to "event complete" handling code.
-
-The bigger problem is that if we can't guarantee clReleaseContext() reaches refcount 0 only in user
-app's thread, we either have to refactor drivers to be able to handle this,
-or solve uninit() in some entirely different way (or ignore it completely).
-
-*/
