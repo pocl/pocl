ec8d97a92407b9d34ad14cf48c1ed8d5ef268c88
Tim Besard <tim.besard@gmail.com>
2024-11-15 14:01:32 +0100

    Fix relative path lookup of device libraries.
    It should include the private library prefix.

--- lib/CL/devices/devices.c
+++ lib/CL/devices/devices.c
@@ -308,6 +308,8 @@ get_pocl_device_lib_path (char *result, char *device_name, int absolute_path)
     }
   else
     {
+       strcat (result, POCL_INSTALL_PRIVATE_LIBDIR_REL);
+       strcat (result, POCL_PATH_SEPARATOR);
 #ifdef _WIN32
       strcat (result, "pocl-devices-");
 #else
