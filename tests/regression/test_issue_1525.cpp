672d907a8078c0e29ebf558eae837206fa38158a
rabijl <robin_bijl@hotmail.nl>
2024-12-20 20:19:15 +0200

    misc: fix build warnings
    Things fixed:
    * accel: couple of header changes
    * cuda: unused function and sign compares
    * llvm-wg: braces in assert
    * test: removal of target ocl version

--- tests/regression/test_issue_1525.cpp
+++ tests/regression/test_issue_1525.cpp
@@ -6,7 +6,6 @@
 
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_MINIMUM_OPENCL_VERSION 110
-#define CL_HPP_TARGET_OPENCL_VERSION 110
 #include <CL/opencl.hpp>
 #include <cassert>
 #include <iostream>
