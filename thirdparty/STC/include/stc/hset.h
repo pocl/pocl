5dd8a4614d31ef6195dd1e93810b876fbee661db
Henry Linjamäki <henry.mikael.linjamaki@intel.com>
2025-05-21 12:40:41 +0300

    Copy headers from STC library

---
+++ thirdparty/STC/include/stc/hset.h
@@ -0,0 +1,43 @@
+/* MIT License
+ *
+ * Copyright (c) 2025 Tyge Løvset
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// Unordered set - implemented with the robin-hood hashing scheme.
+/*
+#define i_type iset,int
+#include "stc/hashset.h"
+#include <stdio.h>
+
+int main(void) {
+    iset set = {0};
+    iset_insert(&set, 5);
+    iset_insert(&set, 8);
+
+    for (c_each(i, iset, set))
+        printf("set %d\n", *i.ref);
+    iset_drop(&set);
+}
+*/
+
+#define _i_prefix hset_
+#define _i_is_set
+#include "hmap.h"
