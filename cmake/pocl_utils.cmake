94c945da6408853459eaf9489f0df648b3993f87
Michal Babej <michal.babej@intel.com>
2025-06-11 19:01:34 +0300

    CMake: path fixes
    * make tests aware of multi-config CMake generators
    * new func: add_symlink_to_built_opencl_dynlib_in_dir()
      will add symlink to OpenCL.dll in other directories than current
    * add symlinks to OpenCL.dll into OpenCL-CTS test directories
      (necessary to force OpenCL-CTS use PoCL on Windows)
    * add symlinks also to libhwloc.dll, if ENABLE_HWLOC

--- cmake/pocl_utils.cmake
+++ cmake/pocl_utils.cmake
@@ -1,7 +1,7 @@
 #=============================================================================
 #   CMake utility functions.
 #
-#   Copyright (c) 2024 Henry Linjam‰ki / Intel Finland Oy
+#   Copyright (c) 2024 Henry Linjam√§ki / Intel Finland Oy
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files (the "Software"), to deal
@@ -48,8 +48,8 @@ function(pocl_assert_symlinks_works)
   message(STATUS "symlinks works.")
 endfunction()
 
-# Creates symlink named OpenCL.dll in the current directory to PoCL
-# built OpenCL runtime library on Windows. This is needed so the
+# Creates symlink named OpenCL.dll (and libhwloc dll) in the given directory
+# to the actually built libpocl(=OpenCL.dll) and libhwloc. This is needed so the
 # internal tests and examples, outside the runtime library directory,
 # link to the PoCL one rather than to a system one.  Adjusting PATH
 # environment doesn't cut because the system libraries are considered
@@ -58,12 +58,12 @@ endfunction()
 #
 # Example usage:
 #
-#   add_symlink_to_built_opencl_dynlib(a b) 
+#   add_symlink_to_built_opencl_dynlib_in_dir("C:\pocl-build\bin" CMake-Target ...)
 #
-# Providing target ensures that the the symlink is created. If a executable X
+# Providing a Target ensures that the the symlink is created. If a executable X
 # is not passed to this function, running a rule - for example, 'make X' -
 # may not create the symlink.
-function(add_symlink_to_built_opencl_dynlib)
+function(add_symlink_to_built_opencl_dynlib_in_dir SYMLINK_DIR DEPENDENCY_TARGET)
   # TODO: Skip the symlink creation if the current directory is the one where
   #       the OpenCL.dll is.
 
@@ -73,22 +73,50 @@ function(add_symlink_to_built_opencl_dynlib)
   endif()
 
   # Name the custom target so that the symlink is created once per directory.
-  set(SYMLINK_TGT_NAME ${CMAKE_CURRENT_BINARY_DIR}/symlink_to_opencl)
+  set(SYMLINK_TGT_NAME ${SYMLINK_DIR}/symlink_target)
   string(REPLACE "/" "_" SYMLINK_TGT_NAME "${SYMLINK_TGT_NAME}")
+  string(REPLACE "\\" "_" SYMLINK_TGT_NAME "${SYMLINK_TGT_NAME}")
   string(REPLACE ":" "_" SYMLINK_TGT_NAME "${SYMLINK_TGT_NAME}")
+  string(REPLACE "-" "_" SYMLINK_TGT_NAME "${SYMLINK_TGT_NAME}")
 
-  if (NOT TARGET ${SYMLINK_TGT_NAME})
-    add_custom_command(
-      OUTPUT OpenCL.dll
-      COMMAND ${CMAKE_COMMAND} -E create_symlink
-      $<TARGET_FILE:${POCL_LIBRARY_NAME}> OpenCL.dll
-      #DEPENDS ${POCL_LIBRARY_NAME}
-      COMMENT "Symbolic link to PoCL-built OpenCL.dll.")
-
-    add_custom_target(${SYMLINK_TGT_NAME} ALL DEPENDS OpenCL.dll)
+  set(SYMLINK_TGT_DIR "${SYMLINK_DIR}")
+  get_property(is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+  if(is_multi_config)
+    set(SYMLINK_TGT_DIR "${SYMLINK_DIR}/$<CONFIG>")
   endif()
 
-  foreach(target IN LISTS ARGN)
-    add_dependencies(${target} ${SYMLINK_TGT_NAME})
-  endforeach()
+  #message(STATUS "Add custom target NAMED: ${SYMLINK_TGT_NAME}")
+  if (NOT TARGET ${SYMLINK_TGT_NAME})
+    #message(STATUS "Add custom command with SYMLINK_DIR: ${SYMLINK_DIR}")
+    add_custom_command(
+      OUTPUT "${SYMLINK_TGT_DIR}/OpenCL.dll"
+	  DEPENDS ${POCL_LIBRARY_NAME}
+      COMMAND ${CMAKE_COMMAND} -E create_symlink
+      "$<TARGET_FILE:${POCL_LIBRARY_NAME}>"  "${SYMLINK_TGT_DIR}/OpenCL.dll"
+      COMMENT "CTS: create symbolic link to PoCL-built OpenCL.dll")
+    set(CUST_TGT_DEPENDS "${SYMLINK_TGT_DIR}/OpenCL.dll")
+
+    if(ENABLE_HWLOC AND TARGET PkgConfig::HWLOC)
+      get_target_property(HWLOC_DLL_FULL_PATH PkgConfig::HWLOC IMPORTED_LOCATION)
+      get_filename_component(HWLOC_DLL_NAME_ONLY "${HWLOC_DLL_FULL_PATH}" NAME)
+      if(HWLOC_DLL_FULL_PATH AND HWLOC_DLL_NAME_ONLY)
+        add_custom_command(OUTPUT "${SYMLINK_TGT_DIR}/${HWLOC_DLL_NAME_ONLY}"
+	  DEPENDS ${POCL_LIBRARY_NAME}
+          COMMAND ${CMAKE_COMMAND} -E create_symlink
+          "${HWLOC_DLL_FULL_PATH}"  "${SYMLINK_TGT_DIR}/${HWLOC_DLL_NAME_ONLY}"
+          COMMENT "CTS: create symbolic link to ${HWLOC_DLL_NAME_ONLY}")
+        list(APPEND CUST_TGT_DEPENDS "${SYMLINK_TGT_DIR}/${HWLOC_DLL_NAME_ONLY}")
+      endif()
+    endif()
+
+    add_custom_target(${SYMLINK_TGT_NAME} DEPENDS ${CUST_TGT_DEPENDS})
+  endif()
+
+  #message(STATUS "adding dep TARGET: ${TARGETS} DEP: ${SYMLINK_TGT_NAME}")
+  add_dependencies(${DEPENDENCY_TARGET} ${SYMLINK_TGT_NAME})
+endfunction()
+
+# creates a symlink in the current binary directory.
+function(add_symlink_to_built_opencl_dynlib TARGETS)
+  add_symlink_to_built_opencl_dynlib_in_dir("${CMAKE_CURRENT_BINARY_DIR}" ${TARGETS})
 endfunction()
