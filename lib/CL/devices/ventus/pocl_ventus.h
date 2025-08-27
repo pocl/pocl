02654dca7aa55deda491b3e0a141b50d21392e92
menxb <men_xiaobin@163.com>
2025-08-27 21:39:02 +0800

    fix(ventus): ensure vt_dev_close is called properly - Add proper cleanup in ventus device close function - Fix potential resource leak in device shutdown

--- lib/CL/devices/ventus/pocl_ventus.h
+++ lib/CL/devices/ventus/pocl_ventus.h
@@ -66,6 +66,7 @@ unsigned int pocl_ventus_probe(struct pocl_device_ops *ops);
 cl_int pocl_ventus_init (unsigned j, cl_device_id dev, const char* parameters);
 void pocl_ventus_run (void *data, _cl_command_node *cmd);
 cl_int pocl_ventus_uninit (unsigned j, cl_device_id device);
+cl_int pocl_ventus_reinit (unsigned j, cl_device_id device);
 void ventus_command_scheduler (struct vt_device_data_t *d);
 void pocl_ventus_submit (_cl_command_node *node, cl_command_queue cq);
 void pocl_ventus_flush (cl_device_id device, cl_command_queue cq);
